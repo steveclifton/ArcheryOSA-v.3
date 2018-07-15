22a209a850e560a42510090d6ec9a55b1a647570
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-15 21:18:34 +1200

    Update fillables

--- app/User.php
+++ app/User.php
@@ -9,13 +9,15 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $primaryKey = 'userid';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'lastipaddress'
+        'firstname', 'lastname', 'username', 'email', 'password', 'lastipaddress', 'roleid'
     ];
 
     /**
