8c10ac899f0a523633d94b56d699cfc3ac06f4cc
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-15 21:20:58 +1200

    Update form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,113 @@
-@extends('layouts.app')
+@extends('template.default')
+
+@section('title')Register @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
-                        @csrf
+    <div class="account-pages"></div>
+    <div class="clearfix"></div>
+    <div class="wrapper-page">
+        <div class="card-box">
+            <div class="panel-heading">
+                <h4 class="text-center"> Register </h4>
+                <i class="fa fa-user-circle loginPerson"></i>
+            </div>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+            <div class="formInput">
+                <form method="POST" class="form-horizontal m-t-20" action="{{ route('register') }}">
+                    @csrf
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                    <div class="form-group ">
+                        <div class="col-12">
+                            <input name="firstname" class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" type="text" required="" placeholder="First Name" value="{{ old('firstname') }}">
+                            @if ($errors->has('firstname'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('firstname') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+
+                    <div class="form-group ">
+                        <div class="col-12">
+                            <input name="lastname" class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" type="text" required="" placeholder="Last Name" value="{{ old('lastname') }}">
+                            @if ($errors->has('lastname'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('lastname') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group ">
+                        <div class="col-12">
+                            <input name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" type="email" required="" placeholder="Email" value="{{ old('email') }}">
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-12">
+                            <input name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password" required="" placeholder="Password">
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-12">
+                            <input name="password_confirmation" class="form-control" type="password" required="" placeholder="Confirm Password">
+                        </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <div class="col-12">
+                            <div class="g-recaptcha"
+                                 data-sitekey="{{env('GOOGLE_RECAPTCHA_KEY')}}">
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    </div>
+                    @if ($errors->has('g-recaptcha-response'))
+                        <div class="col-md-12">
+                            <span style="color: red">
+                                <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+                            </span>
                         </div>
+                    @endif
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="form-group text-center m-t-40">
+                        <div class="col-12">
+                            <button class="btn btn-inverse btn-block text-uppercase waves-effect waves-light"
+                                    type="submit">Register
+                            </button>
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+
+                </form>
+
             </div>
         </div>
+
     </div>
-</div>
+    </div>
+
+
+
+
 @endsection
+
+
