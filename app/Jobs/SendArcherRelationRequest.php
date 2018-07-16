5f2bb32b0cb520492a987dd60b7078c876791fa4
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-16 19:04:03 +1200

    Add job

---
+++ app/Jobs/SendArcherRelationRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\ArcherRelationRequest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendArcherRelationRequest extends ArcheryOSASender implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $email;
+    private $firstname;
+    private $requestusername;
+    private $hash;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($email, $firstname, $requestusername, $hash)
+    {
+        $this->email = $email;
+        $this->firstname = $firstname;
+        $this->requestusername = $requestusername;
+        $this->hash = $hash;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if ($this->checkEmailAddress($this->email)) {
+            Mail::to($this->email)
+                ->send(new ArcherRelationRequest($this->firstname, $this->requestusername, $this->hash));
+        }
+
+    }
+}
