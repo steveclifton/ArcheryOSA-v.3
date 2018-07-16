f644fa775cf2988b5fa771584fa4fdf540cd0f8c
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-16 19:04:18 +1200

    Add job

---
+++ app/Jobs/SendPasswordResetConfirmation.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\PasswordUpdated;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendPasswordResetConfirmation extends ArcheryOSASender implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $email;
+    private $name;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($email, $name)
+    {
+        $this->email = $email;
+        $this->name = $name;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        if ($this->checkEmailAddress($this->email)) {
+            Mail::to($this->email)
+                ->send(new PasswordUpdated($this->name));
+        }
+
+    }
+}
