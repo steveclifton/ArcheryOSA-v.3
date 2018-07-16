fd550383a2ce9b410dca95040d6e3d2d9478d134
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-16 19:03:59 +1200

    Add job

---
+++ app/Jobs/SendArcherRelationConfirm.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\ArcherRelationConfirm;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendArcherRelationConfirm implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $email;
+    private $userfirstname;
+    private $requestuserfirstname;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($email, $userfirstname, $requestuserfirstname)
+    {
+        $this->email = $email;
+        $this->userfirstname = $userfirstname;
+        $this->requestuserfirstname = $requestuserfirstname;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        if ($this->checkEmailAddress($this->email)) {
+            Mail::to($this->email)
+                ->send(new ArcherRelationConfirm($this->userfirstname, $this->requestuserfirstname));
+        }
+
+    }
+}
