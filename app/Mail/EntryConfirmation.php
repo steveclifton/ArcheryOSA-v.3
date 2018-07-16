3d7a86d42da6bcd3360b85544c0162c5bb45097f
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-16 19:04:36 +1200

    Add email

---
+++ app/Mail/EntryConfirmation.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EntryConfirmation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($eventname, $firstname, $eventurl)
+    {
+        $this->eventname = $eventname;
+        $this->firstname = $firstname;
+        $this->eventurl = $eventurl;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject($this->eventname . ' Entry Confirmed!')
+            ->view('emails.entryconfirmed')
+            ->with([
+                'eventname' => $this->eventname,
+                'firstname' => $this->firstname,
+                'eventurl' => $this->eventurl,
+            ]);
+    }
+}
