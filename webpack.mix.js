ec4b77afe9aec0b955fe8f981a1adcd9f3cf7196
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-19 08:24:43 +1200

    Update webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/scssdark/style.scss', 'public/css/scssdark')
+    .sass('resources/assets/sass/scsslight/style.scss', 'public/css/scsslight');
