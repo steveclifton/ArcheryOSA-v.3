4927792dc408c63c4b7cf2788e53a79aa51e5572
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-19 08:24:29 +1200

    Add SASS

--- resources/views/template/styles.blade.php
+++ resources/views/template/styles.blade.php
@@ -1,14 +1,14 @@
 <!-- App css -->
-<link href="{{URL::asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
-<link href="{{URL::asset('/css/icons.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{url('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{url('/css/icons.css')}}" rel="stylesheet" type="text/css" />
 
 <link href="{{URL::asset('/css/archeryosa.css')}}" rel="stylesheet" type="text/css" />
 
 
 @if(Auth::check() && Auth::user()->dark)
-   <link href="{{URL::asset('/css/styledark.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{mix('/css/scssdark/style.css')}}" rel="stylesheet" type="text/css" />
 @else
-   <link href="{{URL::asset('/css/style.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{mix('/css/scsslight/style.css')}}" rel="stylesheet" type="text/css" />
 @endif
 
 <script src="{{URL::asset('/js/modernizr.min.js')}}"></script>
