34e7fed19de99dbf3bd0892f63a92f827cefcccc
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-19 22:23:34 +1200

    Update scripts

--- resources/views/template/scripts.blade.php
+++ resources/views/template/scripts.blade.php
@@ -6,13 +6,24 @@
 <script src="{{URL::asset('/js/jquery.slimscroll.js')}}"></script>
 <script src="{{URL::asset('/js/jquery.scrollTo.min.js')}}"></script>
 
-<script src="{{URL::asset('/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{URL::asset('/plugins/moment/moment.js')}}"></script>
+
+{{--<script src="{{URL::asset('/plugins/timepicker/bootstrap-timepicker.js')}}"></script>--}}
+{{--<script src="{{URL::asset('/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js')}}"></script>--}}
+<script src="{{URL::asset('/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js')}}"></script>
+{{--<script src="{{URL::asset('/plugins/clockpicker/js/bootstrap-clockpicker.min.js')}}"></script>--}}
+{{--<script src="{{URL::asset('/plugins/bootstrap-daterangepicker/daterangepicker.js')}}"></script>--}}
+<script src="{{URL::asset('/pages/jquery.form-pickers.init.js')}}"></script>
+
+
 
 <!-- BEGIN PAGE SCRIPTS -->
-<script src="{{URL::asset('/plugins/moment/moment.js')}}"></script>
+
 {{--<script src='{{URL::asset('/plugins/fullcalendar/js/fullcalendar.min.js')}}'></script>--}}
 {{--<script src="assets/pages/jquery.fullcalendar.js"></script>--}}
 
 <!-- App js -->
 <script src="{{URL::asset('/js/jquery.core.js')}}"></script>
 <script src="{{URL::asset('/js/jquery.app.js')}}"></script>
+
+{{--<script src="{{URL::asset('/plugins/jquery-ui/jquery-ui.min.js')}}"></script>--}}
