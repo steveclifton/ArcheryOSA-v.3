33728f7f25be9de09c4caf8b7e8122ba4457b291
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-19 22:22:41 +1200

    Add profile update method

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -2,18 +2,75 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Requests\UserUpdateProfile;
+use Faker\Provider\DateTime;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    public function get()
+
+
+
+
+
+
+
+    /******************************************************************************
+     * GET Requests
+     ******************************************************************************/
+
+    /**
+     * Gets the users dashboard
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function getDashboard()
     {
         return view('profile.profile');
     }
 
+    /**
+     * Gets the users details form
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getMyDetails()
     {
         return view('profile.mydetails');
     }
+
+
+
+
+
+    /******************************************************************************
+     * POST Requests
+     ******************************************************************************/
+
+    /**
+     * Updates a Users Profile
+     * @param UserUpdateProfile $request
+     * @return redirect
+     */
+    public function updateProfile(UserUpdateProfile $request)
+    {
+        $validated = $request->validated();
+
+        $user = Auth::user();
+        $user->firstname   = $validated['firstname'];
+        $user->lastname    = $validated['lastname'];
+        $user->phone       = $validated['phone'];
+        $user->address     = $validated['address'];
+        $user->city        = $validated['city'];
+        $user->postcode    = $validated['postcode'];
+        $user->postcode    = $validated['postcode'];
+        $user->dateofbirth = $validated['dateofbirth'];
+
+        $user->save();
+
+        return redirect()->back()->with('success', 'Profile Updated');
+    }
+
+
+
 }
