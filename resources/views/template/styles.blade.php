cd50a6afaa163c89f66134d027a4d7c09cb40084
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-19 22:23:28 +1200

    Update styles

--- resources/views/template/styles.blade.php
+++ resources/views/template/styles.blade.php
@@ -1,9 +1,16 @@
 <!-- App css -->
-<link href="{{url('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
-<link href="{{url('/css/icons.css')}}" rel="stylesheet" type="text/css" />
+
 
 <link href="{{URL::asset('/css/archeryosa.css')}}" rel="stylesheet" type="text/css" />
 
+{{--<link href="{{URL::asset('/plugins/timepicker/bootstrap-timepicker.min.css')}}" rel="stylesheet">--}}
+{{--<link href="{{URL::asset('/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css')}}" rel="stylesheet">--}}
+<link href="{{URL::asset('/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css')}}" rel="stylesheet">
+{{--<link href="{{URL::asset('/plugins/clockpicker/css/bootstrap-clockpicker.min.css')}}" rel="stylesheet">--}}
+{{--<link href="{{URL::asset('/plugins/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">--}}
+
+<link href="{{url('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{url('/css/icons.css')}}" rel="stylesheet" type="text/css" />
 
 @if(Auth::check() && Auth::user()->dark)
    <link href="{{mix('/css/scssdark/style.css')}}" rel="stylesheet" type="text/css" />
