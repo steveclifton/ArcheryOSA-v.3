16729d4ed4db4c4ca79c93a1b1b6c60181b5dd5e
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-20 21:20:14 +1200

    Add Controller

---
+++ app/Http/Controllers/Admin/OrganisationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OrganisationController extends Controller
+{
+
+
+
+    /**
+     * GET Methods
+     */
+
+    /**
+     * Return main view
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function get()
+    {
+        return view('admin.organisations.organisations');
+    }
+
+
+    /**
+     * Returns the create view
+     */
+    public function getCreateView()
+    {
+        dd('here');
+    }
+
+}
