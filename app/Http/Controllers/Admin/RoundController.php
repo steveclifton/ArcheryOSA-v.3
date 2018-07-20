b088fd10ac61d1ce4da84b9c36ede0a429bdb9f6
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-20 21:20:10 +1200

    Add Controller

---
+++ app/Http/Controllers/Admin/RoundController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RoundController extends Controller
+{
+
+
+
+    /**
+     * GET Methods
+     */
+
+    /**
+     * Return main view
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function get()
+    {
+        return view('admin.rounds.rounds');
+    }
+
+
+    /**
+     * Returns the create view
+     */
+    public function getCreateView()
+    {
+        dd('here');
+    }
+
+
+}
