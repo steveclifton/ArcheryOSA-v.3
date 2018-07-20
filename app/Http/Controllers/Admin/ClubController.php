a68e829228d5da2b952a1abb31e930d345138ef0
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-20 21:20:21 +1200

    Add Controller

---
+++ app/Http/Controllers/Admin/ClubController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ClubController extends Controller
+{
+
+
+
+    /**
+     * GET Methods
+     */
+
+
+    /**
+     * Returns the main view
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function get()
+    {
+        return view('admin.clubs.clubs');
+    }
+
+
+    /**
+     * Returns the create view
+     */
+    public function getCreateView()
+    {
+        dd('here');
+    }
+}
