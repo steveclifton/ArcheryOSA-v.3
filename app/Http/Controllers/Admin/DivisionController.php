fef0418dedeaf5745e5b9c04b6c7d5483b66668d
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-20 21:20:18 +1200

    Add Controller

---
+++ app/Http/Controllers/Admin/DivisionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DivisionController extends Controller
+{
+
+
+
+    /**
+     * GET Methods
+     */
+
+    /**
+     * Returns main view
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function get()
+    {
+        return view('admin.divisions.divisions');
+    }
+
+
+    /**
+     * Returns the create view
+     */
+    public function getCreateView()
+    {
+        dd('here');
+    }
+}
