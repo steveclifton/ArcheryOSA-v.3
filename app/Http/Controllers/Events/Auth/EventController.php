d173ce38864a6a6f031b3ab8abc00142b0189eb9
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-25 21:02:34 +1200

    Add Controller

---
+++ app/Http/Controllers/Events/Auth/EventController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Events\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EventController extends Controller
+{
+    public function getAllEvents()
+    {
+        // get all the events the user can manage
+        return view('events.auth.events');
+    }
+}
