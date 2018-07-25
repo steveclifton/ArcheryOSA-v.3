de119bd8a4cf20e89bbce2f41675b113f58c3958
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-25 21:04:34 +1200

    Add View

---
+++ resources/views/profile/public/profile.blade.php
@@ -0,0 +1,8 @@
+@extends('template.default')
+
+@section ('title') @endsection
+
+@section('content')
+    public profile,
+    http://coderthemes.com/ubold/dark_hori/extra-profile.html
+@endsection
