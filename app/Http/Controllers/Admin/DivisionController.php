d38798d3f98c8ed4ec3e9fa0f1ed7207e2821ec1
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-27 21:16:17 +1200

    Add Create methods

--- app/Http/Controllers/Admin/DivisionController.php
+++ app/Http/Controllers/Admin/DivisionController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Requests\Admin\CreateDivision;
+use App\Models\Division;
+use App\Models\Organisation;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DivisionController extends Controller
 {
@@ -20,7 +24,8 @@ class DivisionController extends Controller
      */
     public function get()
     {
-        return view('admin.divisions.divisions');
+        $divisions = Division::get();
+        return view('admin.divisions.divisions', compact('divisions'));
     }
 
 
@@ -29,7 +34,33 @@ class DivisionController extends Controller
      */
     public function getCreateView()
     {
-        return view('admin.divisions.create');
+        $organisations = Organisation::get();
+        return view('admin.divisions.create', compact('organisations'));
+    }
+
+
+
+    /******************************************************************************
+     * POST Requests
+     ******************************************************************************/
+    public function createDivision(CreateDivision $request)
+    {
+        $validated = $request->validated();
+
+
+        $club = new Division();
+        $club->label          = ucwords($validated['label']);
+        $club->organisationid = intval($validated['organisationid']);
+        $club->code           = !empty($validated['code'])            ? strtolower($validated['code']) : null;
+        $club->description    = !empty($validated['description'])     ? strtolower($validated['description']) : null;
+        $club->visible        = !empty($validated['visible'])         ? 1 : 0;
+        $club->createdby      = Auth::id();
+        $club->save();
+
+        return redirect('/admin/divisions');
+
 
     }
+
+
 }
