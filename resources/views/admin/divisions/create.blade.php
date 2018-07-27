62503fcdafbe9fcdb349bfaa0a0cd1981daf7298
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-27 21:11:12 +1200

    Update create view

--- resources/views/admin/divisions/create.blade.php
+++ resources/views/admin/divisions/create.blade.php
@@ -17,49 +17,62 @@
         <div class="card-box">
             <h4 class="m-t-0 m-b-30 text-center addFormHeader header-title">Create New Division</h4>
 
-            <form class="form-horizontal myForms" role="form">
-            	 <div class="form-group row">
+            <form class="form-horizontal myForms" method="POST" action="/admin/divisions/create" role="form">
+                @csrf
+                <div class="form-group row">
                     <label for="inputOrgName3" class="col-sm-12 col-md-3 col-form-label">Division*</label>
                     <div class="col-md-9">
-                        <input type="OrgName" class="form-control" id="inputOrgName3">
+                        <input name="label" type="text"
+                               class="form-control{{ $errors->has('label') ? ' is-invalid' : '' }}"
+                               id="inputOrgName3" value="{{old('label')}}">
+                        @if ($errors->has('label'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('label') }}</strong>
+                            </span>
+                        @endif
                     </div>
                 </div>
-               <div class="form-group row">
+
+                <div class="form-group row">
 	                <label class="col-sm-12 col-md-3 col-form-label">Parent Organisation</label>
 	                <div class="col-md-9">
-	                    <select class="form-control">
-	                        <option>Archery NZ</option>
-	                        <option>Auckland District Archery Association</option>
-	                        <option>ECBOPAA</option>
-	                        <option>None</option>
-	                        <option>NZFAA</option>
-	                        <option>World Archery</option>
+	                    <select name="organisationid" class="form-control">
+                            <option value="0">None</option>
+                            @foreach($organisations as $organisation)
+                                <option value="{{$organisation->organisationid}}"
+                                    {!! old('organisationid') == $organisation->organisationid ? 'selected' : '' !!}>
+                                    {{$organisation->label}}
+                                </option>
+	                        @endforeach
 	                    </select>
 	                    <span class="help-block"><small>Select an organisation the division belongs to</small></span>
 	                </div>
 	            </div>
-	             <div class="form-group row">
-                    <label class="col-sm-12 col-md-3 col-form-label">Age Range</label>
-                    <div class="col-md-9">
-                        <input type="text" class="form-control">
-                    </div>
-                </div>
+
                  <div class="form-group row">
-                    <label class="col-sm-12 col-md-3 col-form-label">Code</label>
+                    <label class="col-sm-12 col-md-3 col-form-label">Code*</label>
                     <div class="col-md-9">
-                        <input type="text" class="form-control">
+                        <input name="code" type="text"
+                               class="form-control{{ $errors->has('code') ? ' is-invalid' : '' }}" value="{{old('code')}}">
+                        @if ($errors->has('code'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('code') }}</strong>
+                            </span>
+                        @endif
                     </div>
                 </div>
+
                 <div class="form-group row">
-                    <label class="col-sm-12 col-md-3 col-form-label">Description*</label>
+                    <label class="col-sm-12 col-md-3 col-form-label">Description</label>
                     <div class="col-md-9">
-                        <textarea class="form-control" rows="5"></textarea>
+                        <textarea name="description" class="form-control" rows="5">{{old('description')}}</textarea>
                     </div>
                 </div>
+
                 <div class="form-group row justify-content-end">
                     <div class=" col-md-9">
                         <div class="checkbox checkbox-primary">
-                            <input id="checkbox2" type="checkbox">
+                            <input name="visible" id="checkbox2" type="checkbox" checked>
                             <label for="checkbox2">
                                 Visible
                             </label>
@@ -72,9 +85,6 @@
                     <div class="col-3">
                         <button type="submit" class="myButton btn btn-inverse btn-info waves-effect waves-light">Create</button>
                     </div>
-                   {{--  <div class="col-3">
-                        <button type="submit" class="myButton btn btn-danger btn-info waves-effect waves-light">Delete</button>
-                    </div> --}}
                 </div>
                
             </form>
