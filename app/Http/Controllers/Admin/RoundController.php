ee7bf4c61c01010ce313ff8cb949cd7c61f50f3d
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-27 16:04:57 +1200

    Add create method

--- app/Http/Controllers/Admin/RoundController.php
+++ app/Http/Controllers/Admin/RoundController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Requests\Admin\CreateRound;
+use App\Models\Organisation;
+use App\Models\Round;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class RoundController extends Controller
 {
@@ -20,7 +24,8 @@ class RoundController extends Controller
      */
     public function get()
     {
-        return view('admin.rounds.rounds');
+        $rounds = Round::get();
+        return view('admin.rounds.rounds', compact('rounds'));
     }
 
 
@@ -29,9 +34,47 @@ class RoundController extends Controller
      */
     public function getCreateView()
     {
-        return view('admin.rounds.create');
+        $organisations = Organisation::get();
+
+        return view('admin.rounds.create', compact('organisations'));
 
     }
+
+
+
+
+    /******************************************************************************
+     * POST Requests
+     ******************************************************************************/
+
+    public function createRound(CreateRound $request)
+    {
+        $validated = $request->validated();
+
+        $club = new Round();
+        $club->label          = ucwords($validated['label']);
+        $club->organisationid = intval($validated['organisationid']);
+        $club->code           = !empty($validated['code'])       ? strtolower($validated['code']) : null;
+        $club->dist1          = !empty($validated['dist1'])      ? intval($validated['dist1'])    : null;
+        $club->dist1max       = !empty($validated['dist1max'])   ? intval($validated['dist1max']) : null;
+        $club->dist2          = !empty($validated['dist2'])      ? intval($validated['dist2'])    : null;
+        $club->dist2max       = !empty($validated['dist2max'])   ? intval($validated['dist2max']) : null;
+        $club->dist3          = !empty($validated['dist3'])      ? intval($validated['dist3'])    : null;
+        $club->dist3max       = !empty($validated['dist3max'])   ? intval($validated['dist3max']) : null;
+        $club->dist4          = !empty($validated['dist4'])      ? intval($validated['dist4'])    : null;
+        $club->dist4max       = !empty($validated['dist4max'])   ? intval($validated['dist4max']) : null;
+        $club->totalmax       = !empty($validated['totalmax'])   ? intval($validated['totalmax']) : null;
+        $club->visible        = !empty($validated['visible'])    ? 1 : 0;
+        $club->createdby      = Auth::id();
+        $club->save();
+
+
+        return redirect('/admin/rounds');
+
+
+    }
+
+
 
 
 }
