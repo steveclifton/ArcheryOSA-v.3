5edddb41b1755128a5b9d8f0ad713fddc68b76df
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-27 16:04:03 +1200

    Add method to model

--- app/Models/Round.php
+++ app/Models/Round.php
@@ -8,4 +8,23 @@ class Round extends Model
 {
     protected $table = 'rounds';
     protected $primaryKey = 'roundid';
+
+    public function getDistances()
+    {
+        $distances = [];
+        if (!empty($this->dist1)) {
+            $distances[] = $this->dist1;
+        }
+        if (!empty($this->dist2)) {
+            $distances[] = $this->dist2;
+        }
+        if (!empty($this->dist3)) {
+            $distances[] = $this->dist3;
+        }
+        if (!empty($this->dist4)) {
+            $distances[] = $this->dist4;
+        }
+
+        return $distances;
+    }
 }
