5354948816505e2e9119044faff38fc05a155e81
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-28 16:20:20 +1200

    Add create methods

--- app/Http/Controllers/Admin/CompetitionController.php
+++ app/Http/Controllers/Admin/CompetitionController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Admin\CreateCompetition;
+use App\Models\Competition;
+use App\Models\CompetitionRound;
+use App\Models\Organisation;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CompetitionController extends Controller
 {
@@ -19,7 +24,8 @@ class CompetitionController extends Controller
      */
     public function get()
     {
-        return view('admin.competitions.competitions');
+        $competitions = Competition::get();
+        return view('admin.competitions.competitions', compact('competitions'));
     }
 
 
@@ -28,8 +34,73 @@ class CompetitionController extends Controller
      */
     public function getCreateView()
     {
-        return view('admin.competitions.create');
+
+        // Create the array for mapped rounds
+        $rounds = DB::select("
+            SELECT r.*, o.label as orgname
+            FROM `rounds` r
+            LEFT JOIN `organisations` o ON (r.organisationid = o.organisationid)
+        ");
+
+        $mappedrounds = [];
+        foreach ($rounds as $round) {
+            $orgname = !empty($round->orgname) ? $round->orgname : 'Other';
+
+            $roundtype = 'Outdoor';
+            if ($round->type == 'i') {
+                $roundtype = 'Indoor';
+            }
+            else if ($round->type == 'f') {
+                $roundtype = 'Field';
+            }
+            else if ($round->type == 'c') {
+                $roundtype = 'Clout';
+            }
+            $mappedrounds[$orgname][$roundtype][] = $round;
+        }
+
+        // get all the organisations
+        $organisations = Organisation::get();
+
+        return view('admin.competitions.create', compact('mappedrounds', 'organisations'));
 
     }
+
+
+
+
+
+    /******************************************************************************
+     * POST Requests
+     ******************************************************************************/
+
+    public function createCompetition(CreateCompetition $request)
+    {
+        $validated = $request->validated();
+        $roundids = !empty($validated['roundids']) ? explode(',', $validated['roundids']) : [];
+
+        $competition = new Competition();
+        $competition->label          = ucwords($validated['label']);
+        $competition->organisationid = intval($validated['organisationid']);
+        $competition->description    = !empty($validated['description']) ? $validated['visible'] : null;
+        $competition->visible        = !empty($validated['visible'])     ? 1 : 0;
+        $competition->type           = !empty($validated['type'])        ? strtolower($validated['type'])  : 'o';
+        $competition->createdby      = Auth::id();
+        $competition->save();
+
+        // foreach of the rounds, create a competition-round relationship
+        foreach ($roundids as $roundid) {
+            if (empty($roundid)) {
+                continue;
+            }
+            $competitionround = new CompetitionRound();
+            $competitionround->competitionid = $competition->competitionid;
+            $competitionround->roundid       = $roundid;
+            $competitionround->save();
+        }
+
+        return redirect('/admin/competitions')->with('success', 'Competition created!');
+    }
+
 
 }
