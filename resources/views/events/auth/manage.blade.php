ce620c47d2181b152b079df9bb0ffe459a4d5ed6
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-28 17:23:48 +1200

    Add templete

--- resources/views/events/auth/manage.blade.php
+++ resources/views/events/auth/manage.blade.php
@@ -3,5 +3,62 @@
 @section ('title')Manage Event @endsection
 
 @section('content')
-    something similar to the users profile
+
+    <div class="row">
+
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <a href="#">
+                <div class="db-social-box">
+                    <span class="fa fa-address-book-o"></span>
+                    <h5>Edit Event Details</h5>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <a href="#">
+                <div class="db-social-box">
+                    <span class="fa fa-address-book"></span>
+                    <h5>Edit Competitions</h5>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <a href="#">
+                <div class="db-social-box">
+                    <span class="fa fa-files-o"></span>
+                    <h5>Event Entries</h5>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <a href="#">
+                <div class="db-social-box">
+                    <span class="fa fa-child"></span>
+                    <h5>Event Communications</h5>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <a href="#">
+                <div class="db-social-box">
+                    <span class="fa fa-calendar"></span>
+                    <h5>Event Scoring</h5>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <a href="#">
+                <div class="db-social-box">
+                    <span class="fa fa-bullseye"></span>
+                    <h5>Exports</h5>
+                </div>
+            </a>
+        </div>
+
+    </div>
 @endsection
