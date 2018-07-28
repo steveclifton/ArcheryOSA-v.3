743b47a5edbd47cb452351be1f89a770966cfcf7
Holly <holly@Hollys-Macbook.local>
2018-07-29 11:56:28 +1200

    my events

--- resources/views/profile/auth/events/myevents.blade.php
+++ resources/views/profile/auth/events/myevents.blade.php
@@ -3,5 +3,106 @@
 @section ('title') @endsection
 
 @section('content')
-    list of my events, sorted by upcoming and previous
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title">Upcoming Events</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card-box">
+
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead class="thead-light">
+                        <tr>
+                            <th>Name</th>
+                            <th>Location</th>
+                            <th>Enteries Close</th>
+                            <th>Start</th>
+                            <th>Status</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">2018 Indoor League Series</th>
+                            <td>Nationwide</td>
+                            <td></td>
+                            <td>07-05-2018</td>
+                            <td class="text-success">Open</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">2018 ADAA Indoor Championships</th>
+                            <td>MGAC Indoor Range, 149 Royal Road, Massey, Auckland</td>
+                            <td>13-07-2018</td>
+                            <td>21-07-2018</td>
+                            <td class="text-danger">Enteries Closed</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>21-07-2018</td>
+                            <td>@twitter</td>
+                            <td>@twitter</td>
+
+                        </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title">Previous Events</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card-box">
+
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead class="thead-light">
+                        <tr>
+                            <th>Name</th>
+                            <th>Location</th>
+                            <th>Dates</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">2018 Indoor League Series</th>
+                            <td>Nationwide</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2018 ADAA Indoor Championships</th>
+                            <td >MGAC Indoor Range, 149 Royal Road, Massey, Auckland</td>
+                            <td>13-07-2018</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>21-07-2018</td>
+                        </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
 @endsection
