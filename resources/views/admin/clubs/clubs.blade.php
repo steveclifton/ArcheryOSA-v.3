d87dd610605c5df64702c3b0007e1adeb848636f
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-28 16:19:20 +1200

    Add flash

--- resources/views/admin/clubs/clubs.blade.php
+++ resources/views/admin/clubs/clubs.blade.php
@@ -35,7 +35,7 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="card-box">
-
+                @include('template.alerts')
                 <div class=" myTable table-responsive">
                     <table class="table table-hover">
                         <thead class="thead-light">
