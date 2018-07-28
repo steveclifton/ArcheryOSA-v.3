6d8021ea142f9947442dccacef5f6c153207f27a
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-28 16:15:02 +1200

    Update table name

--- app/Models/Competition.php
+++ app/Models/Competition.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Competition extends Model
 {
-    protected $table = 'competition';
+    protected $table = 'competitions';
     protected $primaryKey = 'competitionid';
 }
