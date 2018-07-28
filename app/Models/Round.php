fb0c4c2e3e0ad4f98d2d2a5ab9009f0d291289cd
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-28 16:14:56 +1200

    Update Model

--- app/Models/Round.php
+++ app/Models/Round.php
@@ -9,6 +9,31 @@ class Round extends Model
     protected $table = 'rounds';
     protected $primaryKey = 'roundid';
 
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+
+    }
+
+    public function getTypeName()
+    {
+        $typename = 'Outdoor';
+
+        if ($this->type == 'i') {
+            $typename = 'Indoor';
+        }
+        else if ($this->type == 'f') {
+            $typename = 'Field';
+        }
+        else if ($this->type == 'c') {
+            $typename = 'Clout';
+        }
+
+        return $typename;
+    }
+
     public function getDistances()
     {
         $distances = [];
@@ -27,4 +52,6 @@ class Round extends Model
 
         return $distances;
     }
+
+
 }
