42bb763d697819fed3bb9a81daedce2af76d0bfe
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-29 14:06:09 +1200

    Add events

--- app/Http/Controllers/Events/Auth/EventController.php
+++ app/Http/Controllers/Events/Auth/EventController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers\Events\Auth;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class EventController extends Controller
 {
     public function getAllEvents()
     {
         // get all the events the user can manage
+        $events = DB::select("
+            SELECT *
+            FROM `events` e
+            JOIN `eventadmins` ea USING (`eventid`)
+            WHERE `ea`.`userid` = :userid
+        ", ['userid' => Auth::id()]);
+        
         return view('events.auth.events');
     }
 
