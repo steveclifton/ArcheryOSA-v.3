de64b8095e1ae8b50ae1e6e102a343ed02624d7c
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-29 13:00:39 +1200

    Add methods to update

--- app/Http/Controllers/Admin/OrganisationController.php
+++ app/Http/Controllers/Admin/OrganisationController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\Admin\CreateOrganisation;
+use App\Http\Requests\Admin\UpdateOrganisation;
 use App\Models\Organisation;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Request;
 
 class OrganisationController extends Controller
 {
@@ -37,7 +39,18 @@ class OrganisationController extends Controller
     }
 
 
+    public function getUpdateView(Request $request)
+    {
+        $organisationid = $request->organisationid ?? null;
+        if (empty($organisationid)) {
+            return redirect('/admin/organisations');
+        }
 
+        $organisation = Organisation::where('organisationid', $organisationid)->get()->first();
+
+        return view('admin.organisations.update', compact('organisation'));
+
+    }
 
 
 
@@ -66,5 +79,30 @@ class OrganisationController extends Controller
 
 
     }
+
+    public function updateOrganisation(UpdateOrganisation $request)
+    {
+        $validated = $request->validated();
+
+        $organisation = Organisation::where('organisationid', $request->organisationid ?? null)->get()->first();
+
+        if (empty($organisation)) {
+            return redirect('/admin/organisations')->with('failure', 'Unable to update');
+        }
+
+        $organisation->label          = ucwords($validated['label']);
+        $organisation->description    = !empty($validated['description']) ? $validated['description'] : null;
+        $organisation->phone          = !empty($validated['phone']) ? $validated['phone'] : null;
+        $organisation->contactname    = !empty($validated['contactname']) ? $validated['contactname'] : null;
+        $organisation->url            = !empty($validated['url']) ? $validated['url'] : null;
+        $organisation->email          = !empty($validated['email']) ? $validated['email'] : null;
+        $organisation->visible        = !empty($validated['visible']) ? 1 : 0;
+        $organisation->save();
+
+        return redirect('/admin/organisations')->with('success', 'Organisation Updated!');
+
+
+    }
+
 
 }
