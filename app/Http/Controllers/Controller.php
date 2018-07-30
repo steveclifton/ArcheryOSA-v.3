fdb340da2a4907e465a95e9a24a4e533dc8b96f7
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-31 08:53:30 +1200

    Add core methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function createHash()
+    {
+        return substr(md5(time() . rand(0, 999)), 0, 5);
+    }
+
+
+    public function prepurl($url)
+    {
+        $url = str_replace(['+', ' '], '-', strtolower(strval($url)));
+        $url = preg_replace(['/[^0-9a-z\-]/', '/-+/'], ['', '-'], $url);
+        return trim($url, '-');
+    }
 }
