236a7696ffaaec9d024f5f8e51d964c6fcbbfa0e
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-07-31 08:52:29 +1200

    Update

--- resources/views/events/auth/events.blade.php
+++ resources/views/events/auth/events.blade.php
@@ -4,72 +4,72 @@
 
 @section('content')
     <div class="row">
-    <div class="col-sm-12">
-        <div class="page-title-box">
-            <h4 class="page-title">Events</h4>
-        </div>
-        <div class="container" style="margin-left: 0; padding-left: 0;">
-            <div class="row">
-                <div class="col-xs-6">
-                    <div class="form-group row" style="display:block; margin-left: 0; margin-right: 1em; text-align: left;">
-                        <div class="col-md-12">
-                            <input class="form-control" placeholder="Search" type="search" name="search">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title">Events</h4>
+            </div>
+            <div class="container" style="margin-left: 0; padding-left: 0;">
+                <div class="row">
+                    <div class="col-xs-6">
+                        <div class="form-group row" style="display:block; margin-left: 0; margin-right: 1em; text-align: left;">
+                            <div class="col-md-12">
+                                <input class="form-control" placeholder="Search" type="search" name="search">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-xs-6">
-                    <a role="button" href="/admin/clubs/create" class="btn btn-inverse waves-effect waves-light ">
-                        <span class="btn-label">
-                            <i class="fa fa-plus"></i>
-                        </span>Add
-                    </a>
+                    <div class="col-xs-6">
+                        <a role="button" href="/events/create" class="btn btn-inverse waves-effect waves-light ">
+                            <span class="btn-label">
+                                <i class="fa fa-plus"></i>
+                            </span>Add
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
     <div class="row">
-        <div class="col-lg-12">
-            <div class="card-box">
+            <div class="col-lg-12">
+                <div class="card-box">
 
-                <div class=" myTable table-responsive">
-                    <table class="table table-hover">
-                        <thead class="thead-light">
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Description</th>
-                                    <th>Visible</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>42</td>
-                                    <th scope="row"><a href="">Dunedin Archery Club</a></th>
-                                    <td></td>
-                                    <td><i class="fa fa-check"></i></td>
+                    <div class=" myTable table-responsive">
+                        <table class="table table-hover">
+                            <thead class="thead-light">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Description</th>
+                                        <th>Visible</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>42</td>
+                                        <th scope="row"><a href="">Dunedin Archery Club</a></th>
+                                        <td></td>
+                                        <td><i class="fa fa-check"></i></td>
 
-                                </tr>
-                                <tr>
-                                    <td>41</td>
-                                    <th scope="row"><a href="">Rosebank Archery Club</a></th>
-                                    <td></td>
-                                    <td><i class="fa fa-check"></i></td>
+                                    </tr>
+                                    <tr>
+                                        <td>41</td>
+                                        <th scope="row"><a href="">Rosebank Archery Club</a></th>
+                                        <td></td>
+                                        <td><i class="fa fa-check"></i></td>
 
-                                </tr>
-                               <tr>
-                                    <td>40</td>
-                                    <th scope="row"><a href="">Grey Goose Wing Archery Society</a></th>
-                                    <td></td>
-                                    <td><i class="fa fa-check"></i></td>
-                                </tr>
+                                    </tr>
+                                   <tr>
+                                        <td>40</td>
+                                        <th scope="row"><a href="">Grey Goose Wing Archery Society</a></th>
+                                        <td></td>
+                                        <td><i class="fa fa-check"></i></td>
+                                    </tr>
 
-                                </tbody>
-                    </table>
+                                    </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
 
 @endsection
