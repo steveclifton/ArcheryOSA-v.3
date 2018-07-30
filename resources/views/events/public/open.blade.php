aff9526cde1b14e48f05e7a2e9016dbb6b9622a7
Holly <holly@Hollys-Macbook.local>
2018-07-30 19:24:36 +1200

    open events

--- resources/views/events/public/open.blade.php
+++ resources/views/events/public/open.blade.php
@@ -3,5 +3,104 @@
 @section ('title') @endsection
 
 @section('content')
-    show all events here that are open for entry
+<div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title "><a href="/upcomingevents">Upcoming Events</a></h4>
+            </div>
+        </div>
+    </div>
+        <div class="row">
+        <div class="col-12">
+            <div class="card-columns">
+                <a href="/event/details/indoor-league-series">
+                    <div class="card m-b-20">
+                        <img class="card-img-top img-fluid" src="{{URL::asset('/images/archery.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title font-18 mt-0">2018 Indoor League Series</h4>
+                            <p class="card-text">Date : {!! date('d F Y') !!}</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="/event/details/indoor-league-series">
+                    <div class="card m-b-20">
+                        <img class="card-img-top img-fluid" src="{{URL::asset('/images/archery.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title font-18 mt-0">2018 Indoor League Series</h4>
+                            <p class="card-text">Date : {!! date('d F Y') !!}</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="/event/details/indoor-league-series">
+                    <div class="card m-b-20">
+                        <img class="card-img-top img-fluid" src="{{URL::asset('/images/archery.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title font-18 mt-0">2018 Indoor League Series</h4>
+                            <p class="card-text">Date : {!! date('d F Y') !!}</p>
+                        </div>
+                    </div>
+                </a>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Section-Title -->
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title">More Events</h4>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card-box">
+
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead class="thead-light">
+                        <tr>
+                            <th>Name</th>
+                            <th>Location</th>
+                            <th>Enteries Close</th>
+                            <th>Start</th>
+                            <th>Status</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">2018 Indoor League Series</th>
+                            <td>Nationwide</td>
+                            <td></td>
+                            <td>07-05-2018</td>
+                            <td class="text-success">Open</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">2018 ADAA Indoor Championships</th>
+                            <td>MGAC Indoor Range, 149 Royal Road, Massey, Auckland</td>
+                            <td>13-07-2018</td>
+                            <td>21-07-2018</td>
+                            <td class="text-danger">Enteries Closed</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>21-07-2018</td>
+                            <td>@twitter</td>
+                            <td>@twitter</td>
+
+                        </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
