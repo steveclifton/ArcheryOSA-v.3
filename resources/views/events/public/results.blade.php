0059494c4d04187546b1ddb6dd0339fda85cfe92
Holly <holly@Hollys-Macbook.local>
2018-08-01 19:46:05 +1200

    event results page

--- resources/views/events/public/results.blade.php
+++ resources/views/events/public/results.blade.php
@@ -3,5 +3,29 @@
 @section ('title')Event Results @endsection
 
 @section('content')
-    event results here
+        <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title">Events</h4>
+            </div>
+            <div class="container" style="margin-left: 0; padding-left: 0;">
+                <div class="row">
+                    <div class="col-xs-6">
+                        <div class="form-group row" style="display:block; margin-left: 0; margin-right: 1em; text-align: left;">
+                            <div class="col-md-12">
+                                <input class="form-control" placeholder="Search" type="search" name="search">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-6">
+                        <a role="button" href="/events/create" class="btn btn-inverse waves-effect waves-light ">
+                            <span class="btn-label">
+                                <i class="fa fa-plus"></i>
+                            </span>Add
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
