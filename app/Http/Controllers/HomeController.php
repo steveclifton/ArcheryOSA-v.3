c089273a9616cd3345bf5914694d9e74caf3e1f2
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-01 19:56:06 +1200

    Get upcoming events

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $upcomingevents = DB::select("
+            SELECT e.*, es.label as status
+            FROM `events` e
+            JOIN `eventstatus` es USING (`eventstatusid`)
+            WHERE `e`.`end` > now()
+        ");
+
+        return view('home', compact('upcomingevents'));
     }
 }
