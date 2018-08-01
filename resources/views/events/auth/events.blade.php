5375f0b3e4c2e5453fc1431fef010d842b0643da
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-01 20:17:55 +1200

    Update

--- resources/views/events/auth/events.blade.php
+++ resources/views/events/auth/events.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-sm-12">
             <div class="page-title-box">
-                <h4 class="page-title">Events</h4>
+                <h4 class="page-title">Manage Existing Events</h4>
             </div>
             <div class="container" style="margin-left: 0; padding-left: 0;">
                 <div class="row">
@@ -39,16 +39,20 @@
                             <thead class="thead-light">
                                 <tr>
                                     <th>Name</th>
-                                    <th>Description</th>
+                                    <th>Start</th>
+                                    <th>End</th>
+                                    <th>Status</th>
                                     <th>Visible</th>
                                 </tr>
                                 </thead>
                                 <tbody>
                                     @foreach($events as $event)
                                         <tr>
-                                            <th scope="row"><a href="/events/manage/{{$event->eventurl}}">Dunedin Archery Club</a></th>
-                                            <td></td>
-                                            <td><i class="fa fa-check"></i></td>
+                                            <th scope="row"><a href="/events/manage/{{$event->eventurl}}">{{ucwords($event->label)}}</a></th>
+                                            <td>{{date('d F Y', strtotime($event->start))}}</td>
+                                            <td>{{date('d F Y', strtotime($event->end))}}</td>
+                                            <td>{{$event->status}}</td>
+                                            <td>@if($event->visible)<i class="fa fa-check"></i>@endif</td>
                                         </tr>
                                     @endforeach
                                 </tbody>
