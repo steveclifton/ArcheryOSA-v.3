324b634ea2c60355ac605a1f1d4a20e9ba769e1a
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-01 19:56:39 +1200

    Update to create and manage events

--- app/Http/Controllers/Events/Auth/EventController.php
+++ app/Http/Controllers/Events/Auth/EventController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Events\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\Events\CreateEvent;
+use App\Models\Club;
+use App\Models\Event;
+use App\Models\EventAdmin;
+use App\Models\Organisation;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -12,17 +17,92 @@ class EventController extends Controller
     {
         // get all the events the user can manage
         $events = DB::select("
-            SELECT *
+            SELECT e.*, es.label as status
             FROM `events` e
             JOIN `eventadmins` ea USING (`eventid`)
+            JOIN `eventstatus` es USING (`eventstatusid`)
             WHERE `ea`.`userid` = :userid
         ", ['userid' => Auth::id()]);
-        
-        return view('events.auth.events');
+
+        return view('events.auth.events', compact('events'));
     }
 
     public function getEventView()
     {
+
         return view('events.auth.manage');
+    }
+
+    public function getCreateEventView()
+    {
+        // if the user is not logged in, or they are just a regular user
+        // - redirect to the apply to create page
+        if ( !Auth::check() || Auth::user()->roleid > 3) {
+            return view('events.public.apply');
+        }
+
+        $organisations = Organisation::where('visible', 1)->get();
+        $clubs = Club::where('visible', 1)->get();
+        // Do some auth checking here. Can the user create an event?
+        return view('events.auth.management.create', compact('organisations', 'clubs'));
+    }
+
+
+
+
+
+    /***************************************************************************
+     *   POST Requests
+     ***************************************************************************/
+
+
+
+    public function createEvent(CreateEvent $request)
+    {
+        $validated = $request->validated();
+
+        $entryclose = !empty($validated['entryclose']) ? new \DateTime($validated['entryclose']) : null;
+        $startdate  = new \DateTime($validated['start']);
+        $enddate    = new \DateTime($validated['end']);
+
+        $difference = $startdate->diff($enddate)->days;
+
+
+
+
+        $event = new Event();
+        $event->label       = ucwords($validated['label']);
+        $event->hash        = $this->createHash();
+        $event->entryclose  = $entryclose->format('Y-m-d H:i:s');
+        $event->start       = $startdate->format('Y-m-d H:i:s');
+        $event->end         = $enddate->format('Y-m-d H:i:s');
+        $event->daycount    = $difference;
+        $event->contactname = !empty($validated['contactname'])     ? $validated['contactname']   : null;
+        $event->phone       = !empty($validated['phone'])           ? $validated['phone']         : null;
+        $event->email       = !empty($validated['email'])           ? $validated['email']         : null;
+        $event->location    = !empty($validated['location'])        ? $validated['location']      : null;
+        $event->cost        = !empty($validated['cost'])            ? $validated['cost']          : null;
+        $event->bankaccount = !empty($validated['bankaccount'])     ? $validated['bankaccount']   : null;
+        $event->bankreference = !empty($validated['bankreference']) ? $validated['bankreference'] : null;
+        $event->schedule    = !empty($validated['schedule'])        ? $validated['schedule']      : null;
+        $event->info        = !empty($validated['info'])            ? $validated['info']          : null;
+        $event->eventstatusid = 6;
+        $event->createdby   = Auth::id();
+        $event->clubid      = !empty($validated['clubid']) ? $validated['clubid'] : null;
+        $event->organisationid = !empty($validated['organisationid']) ? $validated['organisationid'] : null;
+        $event->save();
+
+        $event->eventurl    = makeurl($validated['label'], $event->eventid);
+        $event->save();
+
+        $eventadmin           = new EventAdmin();
+        $eventadmin->userid   = Auth::id();
+        $eventadmin->eventid  = $event->eventid;
+        $eventadmin->canscore = 1;
+        $eventadmin->canedit  = 1;
+        $eventadmin->save();
+
+        return redirect('/');
+
     }
 }
