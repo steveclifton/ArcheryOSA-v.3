ef277ecf4c0be9a6ba160811966f5b609c312603
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-03 20:23:22 +1200

    Update views

--- resources/views/events/auth/management/competitions.blade.php
+++ resources/views/events/auth/management/competitions.blade.php
@@ -3,5 +3,107 @@
 @section ('title')Competitions @endsection
 
 @section('content')
-    @php debug($event) @endphp
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title"><a href="/events/manage/{{$event->eventurl}}">{{ucwords($event->label)}}</a> > <a href="javascript:;">Competitions</a></h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-8 offset-md-2">
+        <div class="card-box">
+            <h4 class="m-t-0 m-b-30 text-center addFormHeader header-title">Competitions</h4>
+
+            <form class="form-horizontal myForms treeForm" method="POST" action="/admin/competitions/create" role="form">
+                @csrf
+
+                <div class="form-group row">
+                    <label for="inputOrgName3" class="col-sm-12 col-md-3 col-form-label">Event Competition Name*</label>
+                    <div class="col-md-9">
+                        <input name="label" type="text"
+                               class="form-control{{ $errors->has('label') ? ' is-invalid' : '' }}" id="inputOrgName3"
+                               value="{{old('label')}}">
+                        @if ($errors->has('label'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('label') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-sm-12 col-md-3 col-form-label">Description</label>
+                    <div class="col-md-9">
+                        <textarea name="description" class="form-control" rows="5">{{old('description')}}</textarea>
+                    </div>
+                </div>
+
+
+
+                <div class="form-group row">
+                    <label class="col-sm-12 col-md-3 col-form-label">Competitions</label>
+                    <div class="col-md-9">
+                        <div class="">
+                            <div class="card-box">
+                                <h4 class="text-dark header-title m-t-0 m-b-30">Check the competition for this </h4>
+
+                                <div id="checkTree">
+                                    @foreach($mappedcompetitions as $orgname => $roundtype)
+                                        <ul>
+                                            <li data-jstree='{"opened":true, "icon": "ti-angle-right"}'>{{$orgname}}
+                                                <ul>
+                                                    @foreach($roundtype as $roundtype => $type)
+
+                                                        <li data-jstree='{"opened":true, "icon": "ti-angle-right"}'>{{$roundtype}}
+                                                            <ul>
+                                                                @foreach($type as $t)
+                                                                    <li data-roundid="{{$t->competitionid}}" data-jstree='{"icon": "ti-angle-right"}' class="round">{{$t->label}}</li>
+                                                                @endforeach
+                                                            </ul>
+                                                    @endforeach
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div><!-- end col -->
+                    </div>
+                </div>
+
+                <input name="roundids" type="hidden" id="jsfields" value="" />
+
+
+
+                <div class="form-group row justify-content-end">
+                    <div class=" col-md-9">
+                        <div class="checkbox checkbox-primary">
+                            <input name="visible" id="checkbox2" type="checkbox" checked>
+                            <label for="checkbox2">
+                                Visible
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="form-group mb-0 justify-content-start row">
+                    <div class="col-sm-12 col-md-3 col-form-label"></div>
+                    <div class="col-3">
+                        <button type="submit" class="myButton btn btn-inverse btn-info waves-effect waves-light">Create</button>
+                    </div>
+
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    </div>
+
+
+
+
 @endsection
---
+++ resources/views/events/auth/scoringlist.blade.php
@@ -0,0 +1,37 @@
+@extends('template.default')
+
+@section ('title')Open Event Scoring @endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title "><a href="javascript">Open Scoring Events</a></h4>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+
+            <div class="card-columns">
+                @foreach(array_slice($events, 0, 3) as $event)
+                    <a href="javascript:;">
+                        <div class="card m-b-20">
+                            <img class="card-img-top img-fluid" src="{{URL::asset('/images/archery.jpg')}}"
+                                 alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title font-18 mt-0">{{$event->label}}</h4>
+                                @if(!empty($event->entryclose))
+                                    <p class="card-text">Entries Close : {!! date('d F Y', strtotime($event->entryclose)) !!}</p>
+                                @endif
+                                <p class="card-text">Start : {!! date('d F Y', strtotime($event->start)) !!}</p>
+                                <p class="card-text">Status : {{$event->eventstatus}}</p>
+                            </div>
+                        </div>
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    </div>
+
+@endsection
