65c14a6e142be883fad94a13d1e6a15844dd19a8
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-06 11:48:04 +1200

    Add view include

---
+++ resources/views/events/auth/management/includes/competitiontext.blade.php
@@ -0,0 +1,22 @@
+<div class="form-group row">
+    <label for="inputLabel" class="col-sm-12 col-md-3 col-form-label">Competition Name*</label>
+    <div class="col-md-9">
+        <input name="label" type="text"
+               class="form-control{{ $errors->has('label') ? ' is-invalid' : '' }}" id="inputLabel"
+               value="{{old('label') ?? $competition['label']}}">
+        <div id="nameerror" class="alert alert-danger hidden">Please name this competition</div>
+    </div>
+</div>
+
+<div class="form-group row">
+    <label class="col-sm-12 col-md-3 col-form-label">Location</label>
+    <div class="col-md-9">
+        <textarea name="location" class="form-control" rows="5" placeholder="Optional">{{old('location') ?? $competition['location']}}</textarea>
+    </div>
+</div>
+<div class="form-group row">
+    <label class="col-sm-12 col-md-3 col-form-label">Schedule</label>
+    <div class="col-md-9">
+        <textarea name="schedule" class="form-control" rows="5" placeholder="Optional">{{old('schedule') ?? $competition['schedule']}}</textarea>
+    </div>
+</div>
