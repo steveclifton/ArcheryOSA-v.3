8961d7d38937d7fe3874ca44ec2367aa66c5136a
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-06 20:35:35 +1200

    Add entry status model

---
+++ app/Models/EntryStatus.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EntryStatus extends Model
+{
+    protected $primaryKey = 'entrystatusid';
+    protected $table = 'entrystatus';
+
+
+
+}
