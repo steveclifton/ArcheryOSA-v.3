2f62ac2b74f16e950e1e546c16f7c9841cb507a1
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-07 21:26:46 +1200

    Add List of people to enter

---
+++ resources/views/events/public/registration/registrationlist.blade.php
@@ -0,0 +1,79 @@
+@extends('template.default')
+
+@section ('title')Event Registration @endsection
+
+@section('content')
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title">
+                    <a href="/events">Events</a>
+                        <i class="ion-arrow-right-c"></i>
+                    <a href="/event/details/{{$event->eventurl}}">{{ucwords($event->label)}}</a>
+                        <i class="ion-arrow-right-c"></i>
+                    <a href="javascript:;">Registration</a>
+                </h4>
+            </div>
+        </div>
+    </div>
+
+
+        <!-- Section-Title -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="page-title-box">
+                    <h5 class="page-title">Select the Archer you wish to enter into the event</h5>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card-box">
+                    @include('template.alerts')
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead class="thead-light">
+                                <tr>
+                                    <th width="50%">Name</th>
+                                    <th width="40%">Entry Status</th>
+                                    <th>Paid</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row" >
+                                            <a href="/event/registration/{{$event->eventurl}}/{{Auth::user()->username}}">{{Auth::user()->getFullName()}}</a>
+                                        </th>
+
+                                        @php $evententry = Auth::user()->getEventEntry($event->eventid) @endphp
+                                        <td class="text-{{getEntryStatusText($evententry->entrystatusid ?? null)}}">
+                                            {{ Auth::user()->getEventEntryStatus($event->eventid) ?: 'Not Entered'}}
+                                        </td>
+
+                                        <td align="justify">@if(!empty($evententry->paid))<i class="fa fa-check-square-o"></i>@endif</td>
+                                    </tr>
+
+                                    @foreach($relations as $relation)
+                                    <tr>
+                                        <th scope="row" >
+                                            <a href="/event/registration/{{$event->eventurl}}/{{$relation->username}}">{{$relation->getFullName()}}</a>
+                                        </th>
+                                        @php $evententry = $relation->getEventEntry($event->eventid) @endphp
+                                        <td class="text-{{getEntryStatusText($evententry->entrystatusid ?? null)}}">
+                                            {{$relation->getEventEntryStatus($event->eventid) ?: 'Not Entered'}}
+                                        </td>
+
+                                        <td align="justify">@if(!empty($evententry->paid))<i class="fa fa-check-square-o"></i>@endif</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                </div>
+            </div>
+        </div>
+
+
+
+@endsection
