7f1af78648fb9c0b7cb6c7948c415d43703e41c7
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-07 21:24:53 +1200

    Tidy crumbs

--- resources/views/events/auth/management/competitions.blade.php
+++ resources/views/events/auth/management/competitions.blade.php
@@ -8,7 +8,11 @@
         <div class="col-sm-12">
             <div class="page-title-box">
                 <h4 class="page-title">
-                    <a href="/events/manage/{{$event->eventurl}}">{{ucwords($event->label)}}</a> > <a href="javascript:;">Competitions</a>
+                    <a href="/events">Events</a>
+                        <i class="ion-arrow-right-c"></i>
+                    <a href="/events/manage/{{$event->eventurl}}">{{ucwords($event->label)}}</a>
+                        <i class="ion-arrow-right-c"></i>
+                    <a href="javascript:;">Competitions</a>
                 </h4>
             </div>
         </div>
@@ -69,5 +73,5 @@
         </div>
     </div>
 
-    <script src="{{URL::asset('/js/divisions.js')}}"></script>
+    <script src="{{URL::asset('/js/admin/divisions.js')}}"></script>
 @endsection
