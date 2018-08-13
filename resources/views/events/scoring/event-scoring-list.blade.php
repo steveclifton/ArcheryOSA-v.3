ffbcc8c14d32aadfcb605c3485e2294e890f28d0
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-13 20:49:06 +1200

    Add scoring list

---
+++ resources/views/events/scoring/event-scoring-list.blade.php
@@ -0,0 +1,60 @@
+@extends('template.default')
+
+@section ('title'){{$event->label}} Scoring @endsection
+
+@section('content')
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h4 class="page-title">
+                    <a href="/events/manage">Events</a>
+                    <i class="ion-arrow-right-c"></i>
+                    <a href="/events/manage/{{$event->eventurl}}">{{ucwords($event->label)}}</a>
+                    <i class="ion-arrow-right-c"></i>
+                    <a href="javascript:;">Scoring</a>
+                </h4>
+            </div>
+        </div>
+    </div>
+
+    <!-- Section-Title -->
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h5 class="page-title">Select competition you wish to score</h5>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card-box">
+                @include('template.alerts')
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead class="thead-light">
+                        <tr>
+                            <th width="40%">Event Competition</th>
+                            <th width="20%">Date</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($competitions as $date=>$comp)
+                            <tr>
+                                <td scope="row" >
+                                    <a href="/event/manage/scoring/{{$event->eventurl}}/{{$comp->eventcompetitionid}}/{{$comp->competitionid}}">{{$comp->label}}</a>
+                                </td>
+                                <td>
+                                    {{$comp->date}}
+                                </td>
+                            </tr>
+                        @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
