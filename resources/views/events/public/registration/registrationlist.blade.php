8bb3ba1b40bc6a1e1a9a337eaaaf544b8fc664e7
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-17 15:56:45 +1200

    Refactor

--- resources/views/events/public/registration/registrationlist.blade.php
+++ resources/views/events/public/registration/registrationlist.blade.php
@@ -19,60 +19,60 @@
     </div>
 
 
-        <!-- Section-Title -->
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="page-title-box">
-                    <h5 class="page-title">Select the Archer you wish to enter into the event</h5>
-                </div>
+    <!-- Section-Title -->
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <h5 class="page-title">Select the Archer you wish to enter into the event</h5>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card-box">
-                    @include('template.alerts')
-                        <div class="table-responsive">
-                            <table class="table table-hover">
-                                <thead class="thead-light">
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card-box">
+                @include('template.alerts')
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead class="thead-light">
+                            <tr>
+                                <th width="50%">Name</th>
+                                <th width="40%">Entry Status</th>
+                                <th>Paid</th>
+                            </tr>
+                            </thead>
+                            <tbody>
                                 <tr>
-                                    <th width="50%">Name</th>
-                                    <th width="40%">Entry Status</th>
-                                    <th>Paid</th>
+                                    <th scope="row" >
+                                        <a href="/event/registration/{{$event->eventurl}}/{{Auth::user()->username}}">{{Auth::user()->getFullName()}}</a>
+                                    </th>
+
+                                    @php $evententry = Auth::user()->getEventEntry($event->eventid) @endphp
+                                    <td class="text-{{getEntryStatusText($evententry->entrystatusid ?? null)}}">
+                                        {{ Auth::user()->getEventEntryStatus($event->eventid) ?: 'Not Entered'}}
+                                    </td>
+
+                                    <td align="justify">@if(!empty($evententry->paid))<i class="fa fa-check-square-o"></i>@endif</td>
                                 </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row" >
-                                            <a href="/event/registration/{{$event->eventurl}}/{{Auth::user()->username}}">{{Auth::user()->getFullName()}}</a>
-                                        </th>
 
-                                        @php $evententry = Auth::user()->getEventEntry($event->eventid) @endphp
-                                        <td class="text-{{getEntryStatusText($evententry->entrystatusid ?? null)}}">
-                                            {{ Auth::user()->getEventEntryStatus($event->eventid) ?: 'Not Entered'}}
-                                        </td>
+                                @foreach($relations as $relation)
+                                <tr>
+                                    <th scope="row" >
+                                        <a href="/event/registration/{{$event->eventurl}}/{{$relation->username}}">{{$relation->getFullName()}}</a>
+                                    </th>
+                                    @php $evententry = $relation->getEventEntry($event->eventid) @endphp
+                                    <td class="text-{{getEntryStatusText($evententry->entrystatusid ?? null)}}">
+                                        {{$relation->getEventEntryStatus($event->eventid) ?: 'Not Entered'}}
+                                    </td>
 
-                                        <td align="justify">@if(!empty($evententry->paid))<i class="fa fa-check-square-o"></i>@endif</td>
-                                    </tr>
-
-                                    @foreach($relations as $relation)
-                                    <tr>
-                                        <th scope="row" >
-                                            <a href="/event/registration/{{$event->eventurl}}/{{$relation->username}}">{{$relation->getFullName()}}</a>
-                                        </th>
-                                        @php $evententry = $relation->getEventEntry($event->eventid) @endphp
-                                        <td class="text-{{getEntryStatusText($evententry->entrystatusid ?? null)}}">
-                                            {{$relation->getEventEntryStatus($event->eventid) ?: 'Not Entered'}}
-                                        </td>
-
-                                        <td align="justify">@if(!empty($evententry->paid))<i class="fa fa-check-square-o"></i>@endif</td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                </div>
+                                    <td align="justify">@if(!empty($evententry->paid))<i class="fa fa-check-square-o"></i>@endif</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
             </div>
         </div>
+    </div>
 
 
 
