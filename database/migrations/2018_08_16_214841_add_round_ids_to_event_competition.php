7f17defd1c1389ae37ff6422a7d7420585488a2a
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-17 13:21:23 +1200

    Add migration

---
+++ database/migrations/2018_08_16_214841_add_round_ids_to_event_competition.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRoundIdsToEventCompetition extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('eventcompetitions', function($table) {
+            $table->string('roundids')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
