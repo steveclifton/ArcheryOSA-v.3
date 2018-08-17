d49d9cd4090600e916a14aa478b48701e306293d
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-17 15:57:44 +1200

    Add relationship methods

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Requests\User\UserUpdateProfile;
 use App\Http\Controllers\Controller;
+use App\Jobs\SendArcherRelationRequest;
+use App\Models\UserRelation;
+use App\User;
+
 use Illuminate\Support\Facades\Auth;
+Use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
@@ -50,6 +55,115 @@ class ProfileController extends Controller
     }
 
 
+
+    /******************************************************************************
+     * RELATIONSHIPS
+     ******************************************************************************/
+    public function getRelationshipsList()
+    {
+        $relations = UserRelation::where('userid', Auth::id())->pluck('relationid')->toarray();
+
+        if (!empty($relations)) {
+            $relations = User::wherein('userid', $relations)->get();
+            foreach ($relations as $relation) {
+                $relation->status = UserRelation::where('userid', Auth::id())
+                                                ->where('relationid', $relation->userid)
+                                                ->pluck('authorised')->first();
+            }
+        }
+
+
+        return view('profile.auth.relationships-list', compact('relations'));
+    }
+    public function getRelationshipsRequest()
+    {
+        return view('profile.auth.relation', compact('relation'));
+    }
+    public function requestRelationship(Request $request)
+    {
+        $user = User::where('email', $request->input('email', null) ?? '')->get()->first();
+
+        if (empty($user)) {
+            return back()->with('failure', 'Cannot find a user with that email address');
+        }
+
+        // check to see if a relationship already exists
+        $userrelation = UserRelation::where('userid', Auth::id())->where('relationid', $user->userid)->get()->first();
+
+        if (!empty($userrelation)) {
+            return back()->with('failure', 'Relationship already exists');
+        }
+
+        $authfullname = ucwords(Auth::user()->firstname ?? '') . ' ' . ucwords(Auth::user()->lastname ?? '') . '(' . Auth::user()->email . ')';
+
+        $userrelation = new UserRelation();
+        $userrelation->userid = Auth::id();
+        $userrelation->relationid = $user->userid;
+        $userrelation->hash = $this->createHash();
+        $userrelation->save();
+
+        SendArcherRelationRequest::dispatch($user->email,
+                                            $user->firstname,
+                                            $authfullname,
+                                            $userrelation->hash,
+                                            getenv('APP_URL') . '/profile/relationship/authorise'
+        );
+
+        return back()->with('success', 'Request sent!');
+    }
+    public function authoriseRelation(Request $request)
+    {
+        $userrelation = UserRelation::where('hash', $request->hash ?? -1)->where('authorised', 0)->get()->first();
+
+        $status = false;
+
+        if (empty($userrelation)) {
+            $message = 'Authorisation link invalid (may have already been approved)';
+        }
+        else {
+            $userrelation->authorised = 1;
+            $userrelation->save();
+
+            $message = 'Authorisation complate!';
+            $status = true;
+        }
+
+        return view('profile.public.relationconfirm', compact('message', 'status'));
+
+    }
+    public function removeRelationship(Request $request)
+    {
+        $userid = $request->userid;
+        if (empty($userid)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Request, please try again later'
+            ]);
+        }
+
+        $userrelation = UserRelation::where('userid', Auth::id())->where('relationid', $userid)->get()->first();
+        if (empty($userrelation)) {
+            // could the be relation trying to remove their relation with another user, try that
+            $userrelation = UserRelation::where('userid', $userid)->where('relationid', Auth::id())->get()->first();
+        }
+
+        if (empty($userrelation)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Request, please try again later'
+            ]);
+        }
+
+        // Delete the user relationship
+        //$userrelation->delete();
+
+        // return response
+        return response()->json([
+            'success' => true,
+            'message' => 'Relationship Removed'
+        ]);
+
+    }
 
 
 
