c254fc32604b470fc22189bb984e88e48aa765f7
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-18 10:09:43 +1200

    Update

--- resources/views/events/public/apply.blade.php
+++ resources/views/events/public/apply.blade.php
@@ -1,9 +1,23 @@
 @extends('template.default')
 
-@section ('title')Events @endsection
+@section ('title')Account Upgrade @endsection
 
 @section('content')
 
-    Apply to manage events
+
+    <div class="wrapper-page">
+        <div class="ex-page-content text-center">
+
+            <h2>Your account is unable to create an event just yet..</h2>
+            <br>
+            <p class="text-muted">
+                Apply to ArcheryOSA now to upgrade your account!
+            </p>
+            <br>
+            <a class="btn btn-default waves-effect waves-light" href="mailto:archeryosa@gmail.com?Subject=Account%20Upgrade">Apply Now</a>
+
+        </div>
+    </div>
+    <!-- end wrapper page -->
 
 @endsection
