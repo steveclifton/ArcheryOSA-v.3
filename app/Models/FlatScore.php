9134f03ef564bd00566d7ccd64c2f83086e3469f
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-08-28 20:15:35 +1200

    Add model

---
+++ app/Models/FlatScore.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FlatScore extends Model
+{
+    protected $table = 'scores_flat';
+    protected $primaryKey = 'flatscoreid';
+}
