d8e93b6fafe7c46e091be43aceb4d835f67cb7b7
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-09-02 13:44:46 +1200

    Add migrations

---
+++ database/migrations/2018_09_02_014304_add_week_to_scores_flat.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWeekToScoresFlat extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('scores_flat', function($table) {
+            $table->integer('week')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
