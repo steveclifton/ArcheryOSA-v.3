c0952f058499869853561c1f71d06a222f735576
Steve <steve.clifton@blackpepper.co.nz>
2018-09-15 10:28:47 +1200

    Add league creation

--- app/Http/Controllers/Events/Auth/EventCompetitionController.php
+++ app/Http/Controllers/Events/Auth/EventCompetitionController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Events\Auth;
 
 
 use App\Http\Requests\Auth\Competitions\CreateEventCompetition;
+use App\Http\Requests\Auth\Competitions\CreateLeagueCompetition;
 use App\Http\Requests\Auth\Competitions\UpdateEventCompetition;
+use App\Http\Requests\Auth\Competitions\UpdateLeagueCompetition;
+use App\Models\Competition;
+use App\Models\EntryCompetition;
 use App\Models\Event;
 use App\Models\EventAdmin;
 use App\Models\EventCompetition;
+use App\Models\EventEntry;
 use App\Models\ScoringLevel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -58,6 +63,36 @@ class EventCompetitionController extends EventController
             return redirect()->back()->with('failure', 'Event not found');
         }
 
+        // Get mapped rounds
+        $mappedrounds = $this->helper->getMappedRoundTree();
+
+        // Get mapped divisions
+        $mappeddivisions = $this->helper->getMappedDivisionsTree();
+
+        // get all the scoring levels
+        $scoringlevels = ScoringLevel::get();
+
+        // Means the event is a league event
+        $leagueweeks = null;
+        if ($event->eventtypeid == 2) {
+
+            // Add the first competition day to the event
+            $competition = EventCompetition::where('eventid', $event->eventid)
+                ->get()
+                ->first();
+
+            $entries = EntryCompetition::where('eventid', $event->eventid)->get()->first();
+
+            $formaction = empty($competition) ? 'create' : 'update';
+
+            return view('events.auth.management.league.competition',
+                compact('event', 'entries', 'mappedrounds', 'competition', 'scoringlevels', 'leagueweeks', 'formaction', 'mappeddivisions')
+            );
+
+        }
+
+
+
 
         // Get the events daterange
         $event->daterange = $this->helper->getEventsDateRange($event);
@@ -75,26 +110,15 @@ class EventCompetitionController extends EventController
             $competition = $competition->toArray();
         }
 
-        // get the competitions mapped into a tree
-        //$mappedcompetitions = $this->helper->getMappedCompetitionTree();
-
-        $mappedrounds = $this->helper->getMappedRoundTree();
-
-        $mappeddivisions = $this->helper->getMappedDivisionsTree();
-
-        // get all the scoring levels
-        $scoringlevels = ScoringLevel::get();
-
-        // Means the event is a league event
-        $leagueweeks = null;
-        if ($event->eventtypeid == 2) {
-            $leagueweeks = ceil($event->daycount / 7);
-        }
-
         $formaction = empty($competition) ? 'create' : 'update';
 
+        $entries = EntryCompetition::where('eventid', $event->eventid)
+            ->where('eventcompetitionid',  $competition['eventcompetitionid'])
+            ->get()->first();
+
+
         return view('events.auth.management.competitions',
-                compact('event',  'mappedrounds', 'competition', 'scoringlevels', 'leagueweeks', 'formaction', 'mappeddivisions')
+                compact('event', 'entries', 'mappedrounds', 'competition', 'scoringlevels', 'leagueweeks', 'formaction', 'mappeddivisions')
         );
     }
 
@@ -220,4 +244,119 @@ class EventCompetitionController extends EventController
         return redirect()->back()->with('success', 'Competition updated!');
 
     }
+
+
+
+
+
+    // League
+
+    public function createLeagueCompetition(CreateLeagueCompetition $request)
+    {
+        $validated = $request->validated();
+
+        $event = Event::where('eventid', $validated['eventid'] ?? -1)->get()->first();
+
+        if (empty($event)) {
+            return redirect()->back()->with('failure', 'Invalid request');
+        }
+
+        // Get array of roundids
+        $roundids = !empty($validated['roundids']) ? explode(',', $validated['roundids']) : [];
+        $roundids = array_filter($roundids);
+
+        if (empty($roundids) || count($roundids) > 1) {
+            return back()->withinput()->with('failure', 'Please choose only 1 round for the league');
+        }
+
+        $roundid = reset($roundids);
+
+
+        // Get array of divisionids
+        $divisionids = !empty($validated['divisionids']) ? explode(',', $validated['divisionids']) : [];
+        $divisionidsfinal = [];
+        foreach ($divisionids as $did) {
+            if (empty($did)) {
+                continue;
+            }
+            $divisionidsfinal[] = $did;
+        }
+
+
+        $eventcompetition = new EventCompetition();
+        $eventcompetition->eventid          = !empty($validated['eventid'])       ? intval($validated['eventid']) : '';
+        $eventcompetition->label            = !empty($validated['label'])         ? ucwords($validated['label']) : '';
+        $eventcompetition->location         = !empty($validated['location'])      ? $validated['location'] : '';
+        $eventcompetition->schedule         = !empty($validated['schedule'])      ? $validated['schedule'] : '';
+        $eventcompetition->roundids         = intval($roundid);
+        $eventcompetition->currentweek       = 1;
+        $eventcompetition->divisionids      = !empty($divisionidsfinal)           ? json_encode($divisionidsfinal) : json_encode('');
+        $eventcompetition->scoringlevel     = !empty($validated['scoringlevel'])  ? intval($validated['scoringlevel']) : 0;
+        $eventcompetition->ignoregenders    = empty($validated['ignoregenders'])  ? 0 : 1;
+        $eventcompetition->scoringenabled   = empty($validated['scoringenabled']) ? 0 : 1;
+        $eventcompetition->visible          = 1;
+        $eventcompetition->save();
+
+        return redirect()->back()->with('success', 'Competition created!');
+
+    }
+
+    public function updateLeagueCompetition(UpdateLeagueCompetition $request)
+    {
+
+        $validated = $request->validated();
+
+        $event = Event::where('eventid', $validated['eventid'] ?? -1)->get()->first();
+
+        $eventcompetition = EventCompetition::where('eventid', $event->eventid ?? -1)
+                                            ->get()
+                                            ->first();
+
+        if (empty($event) || empty($eventcompetition)) {
+            return redirect()->back()->with('failure', 'Invalid request');
+        }
+
+
+        // Get array of roundids
+        $roundids = !empty($validated['roundids']) ? explode(',', $validated['roundids']) : [];
+        $roundids = array_filter($roundids);
+
+        if (empty($roundids) || count($roundids) > 1) {
+            return back()->withinput()->with('failure', 'Please choose only 1 round for the league');
+        }
+        // get first one
+        $roundid = reset($roundids);
+
+
+        // Get array of divisionids
+        $divisionids = !empty($validated['divisionids']) ? explode(',', $validated['divisionids']) : [];
+        $divisionidsfinal = [];
+        foreach ($divisionids as $did) {
+            if (empty($did)) {
+                continue;
+            }
+            $divisionidsfinal[] = $did;
+        }
+
+
+        $eventcompetition->eventid          = !empty($validated['eventid'])       ? intval($validated['eventid']) : '';
+        $eventcompetition->label            = !empty($validated['label'])         ? ucwords($validated['label']) : '';
+        $eventcompetition->location         = !empty($validated['location'])      ? $validated['location'] : '';
+        $eventcompetition->schedule         = !empty($validated['schedule'])      ? $validated['schedule'] : '';
+        $eventcompetition->roundids         = intval($roundid);
+        $eventcompetition->divisionids      = !empty($divisionidsfinal)           ? json_encode($divisionidsfinal) : json_encode('');
+        $eventcompetition->scoringlevel     = !empty($validated['scoringlevel'])  ? intval($validated['scoringlevel']) : 0;
+        $eventcompetition->ignoregenders = empty($validated['ignoregenders']) ? 0 : 1;
+        $eventcompetition->scoringenabled   = empty($validated['scoringenabled']) ? 0 : 1;
+//        $eventcompetition->visible          = empty($validated['visible'])        ? 0 : 1;
+        $eventcompetition->save();
+
+        return redirect()->back()->with('success', 'Competition updated!');
+
+    }
+
+
+
+
+
 }
