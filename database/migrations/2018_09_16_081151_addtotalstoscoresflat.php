0a8804375062498e06571d1003763dfcd4a73ed8
Steve <steve.clifton@blackpepper.co.nz>
2018-09-17 21:09:53 +1200

    Add migration

---
+++ database/migrations/2018_09_16_081151_addtotalstoscoresflat.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Addtotalstoscoresflat extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('scores_flat', function($table) {
+            $table->integer('max')->default(0);
+            $table->integer('inners')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
