077a927d5b6fbdd2336771f7874fdfed1e80f5bf
Steve <steve.clifton@blackpepper.co.nz>
2018-09-17 21:08:04 +1200

    Add api controller

---
+++ app/Http/Controllers/API/APIEventsController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class APIEventsController extends Controller
+{
+
+    /**
+     * Gets all upcoming events
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getEvents()
+    {
+        $events = DB::select("
+            SELECT e.*, es.label as eventstatus
+            FROM `events` e 
+            JOIN `eventstatus` es USING (`eventstatusid`)
+            WHERE `e`.`end` > NOW()
+            AND `e`.`visible` = 1
+            ORDER BY `e`.`promoted` DESC, IFNULL(e.entryclose, e.start) 
+        ");
+
+        return response()->json([
+            'success' => true,
+            'date' => [
+                'events' => $events
+            ]
+        ]);
+    }
+
+
+    /**
+     * Gets all previous events
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getPreviousEvents()
+    {
+        $events = DB::select("
+            SELECT e.*, es.label as eventstatus
+            FROM `events` e 
+            JOIN `eventstatus` es USING (`eventstatusid`)
+            WHERE `e`.`end` < NOW()
+            AND `e`.`visible` = 1
+            ORDER BY `e`.`promoted` DESC, IFNULL(e.entryclose, e.start) 
+        ");
+
+        return response()->json([
+            'success' => true,
+            'date' => [
+                'events' => $events
+            ]
+        ]);
+    }
+}
