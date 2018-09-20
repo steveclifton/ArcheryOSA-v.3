7440fd5cdc9ecd37f83d1c923d218277044a2ee2
Steve <steve.clifton@blackpepper.co.nz>
2018-09-20 22:04:31 +1200

    Add model

---
+++ app/LeaguePoint.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LeaguePoint extends Model
+{
+    protected $table = 'leaguepoints';
+    protected $primaryKey = 'leaguepointid';
+}
