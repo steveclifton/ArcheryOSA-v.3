b9477a33f8211a9e37b8f654a267e5ec8905103b
Steve <steve.clifton@blackpepper.co.nz>
2018-09-20 22:05:46 +1200

    Add trait for users results

---
+++ app/Http/Traits/UserResults.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Traits;
+
+use App\LeaguePoint;
+use Illuminate\Support\Facades\DB;
+
+trait UserResults
+{
+    private function getUserTop10Scores($userid, $divisionid, $eventid)
+    {
+        $result = DB::select("
+            SELECT sum(`total`) as total
+            FROM `scores_flat`
+            WHERE `userid` = :userid
+            AND `divisionid` = :divisionid
+            AND `eventid` = :eventid
+            ORDER BY `total` DESC
+            LIMIT 10
+            ", ['userid' => $userid, 'divisionid' => $divisionid, 'eventid' => $eventid]);
+
+
+        return !empty($result[0]) ? $result[0] : 0;
+
+    }
+
+    private function getUserAverage($userid, $divisionid, $eventid)
+    {
+        $result = DB::select("
+            SELECT avg(`total`) as average
+            FROM `scores_flat`
+            WHERE `userid` = :userid
+            AND `divisionid` = :divisionid
+            AND `eventid` = :eventid
+            ORDER BY `total` DESC
+            ", ['userid' => $userid, 'divisionid' => $divisionid, 'eventid' => $eventid]);
+
+
+        return !empty($result[0]) ? $result[0] : 0;
+    }
+
+    public static function getUserTop10Points($userid, $divisionid, $eventid)
+    {
+        $result = DB::select("
+            SELECT sum(`points`) as points
+            FROM `leaguepoints`
+            WHERE `userid` = :userid
+            AND `divisionid` = :divisionid
+            AND `eventid` = :eventid
+            ORDER BY `points` DESC
+            LIMIT 10
+            ",['userid'=>$userid, 'divisionid'=>$divisionid, 'eventid' => $eventid]);
+
+        return !empty($result[0]) ? $result[0] : 0;
+    }
+
+    public static function getUserWeekPoints($userid, $divisionid, $eventid, $week)
+    {
+        $result = LeaguePoint::where('userid', $userid)
+            ->where('divisionid', $divisionid)
+            ->where('eventid', $eventid)
+            ->where('week', $week)
+            ->pluck('points')
+            ->first();
+
+        return $result ?? 0;
+    }
+
+}
