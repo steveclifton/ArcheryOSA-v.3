b6bd6092e16ef4e4e14685a94558b0aa3b847bff
Steve <steve.clifton@blackpepper.co.nz>
2018-09-30 19:45:00 +1300

    Add update view

--- app/Http/Controllers/Events/Auth/EventEntryController.php
+++ app/Http/Controllers/Events/Auth/EventEntryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Events\Auth;
 use App\Jobs\SendEntryConfirmation;
 use App\Models\Club;
 use App\Models\Division;
+use App\Models\EntryCompetition;
 use App\Models\Event;
 use App\Models\EventAdmin;
 use App\Models\EventEntry;
@@ -40,8 +41,9 @@ class EventEntryController extends EventController
         $event = $this->event;
 
         $evententries = DB::select("
-            SELECT ee.entryid, CONCAT_WS(' ', ee.firstname, ee.lastname ) as name, ee.confirmationemail, ee.paid, d.label as division, ee.created_at as created, es.label as status  
+            SELECT ee.entryid, u.username, CONCAT_WS(' ', ee.firstname, ee.lastname ) as name, ee.confirmationemail, ee.paid, d.label as division, ee.created_at as created, es.label as status  
             FROM `evententrys` ee
+            JOIN `users` u USING (`userid`)
             JOIN `divisions` d USING (`divisionid`)
             JOIN `entrystatus` es USING (`entrystatusid`)
             WHERE `eventid` = '".$event->eventid."'
@@ -103,6 +105,77 @@ class EventEntryController extends EventController
             compact('user', 'event', 'clubs', 'divisionsfinal', 'competitionsfinal', 'leaguecompround', 'multipledivisions'));
     }
 
+    public function getEventEntryUpdateView(Request $request)
+    {
+
+        $event = Event::where('eventurl', $request->eventurl ?? -1)->get()->first();
+
+        $user = User::where('username', $request->username)->get()->first();
+
+        if (empty($event) || empty($user)) {
+            return back()->with('failure', 'Cannot find user');
+        }
+
+
+        $evententry = EventEntry::where('eventid', $event->eventid ?? -1)
+                        ->where('userid', $user->userid)
+                        ->get()
+                        ->first();
+
+
+        $eventcompetitions = DB::select("
+            SELECT *
+            FROM `eventcompetitions`
+            WHERE `eventid` = :eventid
+        ", ['eventid' => $event->eventid]);
+
+        $leaguecompround = null;
+        if ($event->isLeague()) {
+            $leaguecompround = reset($eventcompetitions);
+            $leaguecompround = $leaguecompround->eventcompetitionid . '-' . $leaguecompround->roundids;
+        }
+
+        $multipledivisions = false;
+        $divisionsfinal    = [];
+        $competitionsfinal = [];
+        foreach ($eventcompetitions as $eventcompetition) {
+
+            if (empty($multipledivisions) && $eventcompetition->multipledivisions) {
+                $multipledivisions = true;
+            }
+
+            $divisions = Division::wherein('divisionid', json_decode($eventcompetition->divisionids))->orderBy('bowtype')->get();
+            foreach ($divisions as $division) {
+                $divisionsfinal[$division->divisionid] = $division;
+            }
+
+            if ($event->isLeague()) {
+                $eventcompetition->rounds = Round::where('roundid', $eventcompetition->roundids)->get();
+            }
+            else {
+                $eventcompetition->rounds = Round::wherein('roundid', json_decode($eventcompetition->roundids))->get();
+            }
+
+            $competitionsfinal[$eventcompetition->date] = $eventcompetition;
+        }
+
+        $clubs = Club::where('visible', 1)->get();
+
+        $entrycompetitions = EntryCompetition::where('entryid', $evententry->entryid)->get();
+
+        $entrycompetitionids = [];
+        foreach ($entrycompetitions as $entrycompetition) {
+            $entrycompetitionids[$entrycompetition->eventcompetitionid][$entrycompetition->roundid] = $entrycompetition->roundid;
+        }
+
+
+        return view('events.auth.management.entries.update',
+                compact('user', 'entrycompetitionids', 'evententry', 'event',
+                        'clubs', 'divisionsfinal', 'competitionsfinal',
+                        'leaguecompround', 'multipledivisions'
+                        )
+                    );
+    }
 
 
     /**********************
