4c759f4a2a0f25212c166316a14f8cfe3275a374
Steve <steve.clifton@outlook.com>
2018-10-17 22:26:42 +1300

    Add label

--- resources/views/events/results/events-list.blade.php
+++ resources/views/events/results/events-list.blade.php
@@ -37,7 +37,7 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="page-title-box">
-                    <h4 class="page-title">Events</h4>
+                    <h4 class="page-title">Event Results</h4>
                 </div>
             </div>
         </div>
