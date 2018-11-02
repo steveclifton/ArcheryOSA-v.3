1cb10384f78462001d1acdeac3c714d33665f09e
Steve <steve.clifton@outlook.com>
2018-11-02 21:18:18 +1300

    Update API

--- app/Http/Controllers/API/APIEventsController.php
+++ app/Http/Controllers/API/APIEventsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\Division;
+use App\Models\EventCompetition;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
@@ -16,18 +18,51 @@ class APIEventsController extends Controller
     public function getUpcomingEvents()
     {
         $events = DB::select("
-            SELECT e.eventid, e.label as eventname, e.eventtypeid, e.entryclose, e.start, e.end, e.daycount, 
+            SELECT e.eventid, e.label as eventname, e.eventtypeid, e.organisationid, e.clubid, e.entryclose, e.start, e.end, e.daycount, 
              e.contactname, e.phone, e.email, e.location, e.cost, e.bankaccount, e.bankreference, e.schedule, e.info,
-              e.eventurl, e.clubid, e.organisationid, e.entrylimit,  es.label as eventstatus
+              e.eventurl, e.organisationid, e.entrylimit,  es.label as eventstatus, c.label as clubname
             FROM `events` e 
             JOIN `eventstatus` es USING (`eventstatusid`)
+            LEFT JOIN `clubs` c ON (e.`clubid` = c.`clubid`)
             WHERE `e`.`end` > NOW()
             AND `e`.`visible` = 1
-            ORDER BY IFNULL(e.entryclose, e.start) 
+            ORDER BY `e`.`start` 
         ");
 
         foreach ($events as $event) {
             $event->eventurl = route('event', $event->eventurl);
+
+            $event->competitions = DB::select("
+                SELECT `eventcompetitionid`,`date`,`label` as eventcompetitionname,`eventid`,`roundids`,
+                        `currentweek`,`location`,`schedule`,`divisionids`
+                FROM `eventcompetitions`
+                WHERE `eventid` = :eventid
+            ", ['eventid' => $event->eventid]);
+
+            if (!empty($event->competitions)) {
+                foreach ($event->competitions as $competition) {
+
+                    $divisionids = json_decode($competition->divisionids);
+                    $divisionids = implode(',', (array) $divisionids);
+                    $competition->divisons = DB::select("
+                        SELECT `divisionid`,`label`,`code`
+                        FROM `divisions`
+                        WHERE `divisionid` IN (". $divisionids .") 
+                    ");
+                    $competition->divisionids = null;
+
+                    $roundids = json_decode($competition->roundids);
+                    $roundids = implode(',', (array) $roundids);
+                    $competition->rounds = DB::select("
+                        SELECT *
+                        FROM `rounds`
+                        WHERE `roundid` IN (". $roundids .")
+                    ");
+                    $competition->roundids = null;
+                }
+
+            }
+
         }
 
         return response()->json([
@@ -46,19 +81,53 @@ class APIEventsController extends Controller
     public function getPreviousEvents()
     {
         $events = DB::select("
-            SELECT e.eventid, e.label as eventname, e.eventtypeid, e.entryclose, e.start, e.end, e.daycount, 
+            SELECT e.eventid, e.label as eventname, e.eventtypeid, e.organisationid, e.clubid, e.entryclose, e.start, e.end, e.daycount, 
               e.contactname, e.phone, e.email, e.location, e.cost, e.bankaccount, e.bankreference, e.schedule, e.info,
-              e.eventurl, e.clubid, e.organisationid, e.entrylimit,  es.label as eventstatus
+              e.eventurl, e.organisationid, e.entrylimit,  es.label as eventstatus, c.label as clubname
             FROM `events` e 
             JOIN `eventstatus` es USING (`eventstatusid`)
+            LEFT JOIN `clubs` c ON (e.`clubid` = c.`clubid`)
             WHERE `e`.`end` < NOW()
             AND `e`.`visible` = 1
-            ORDER BY `e`.`promoted` DESC, IFNULL(e.entryclose, e.start) 
+            ORDER BY `e`.`start` 
         ");
 
 
         foreach ($events as $event) {
             $event->eventurl = route('event', $event->eventurl);
+
+
+            $event->competitions = DB::select("
+                SELECT `eventcompetitionid`,`date`,`label` as eventcompetitionname,`eventid`,`roundids`,
+                        `currentweek`,`location`,`schedule`,`divisionids`
+                FROM `eventcompetitions`
+                WHERE `eventid` = :eventid
+            ", ['eventid' => $event->eventid]);
+
+            if (!empty($event->competitions)) {
+                foreach ($event->competitions as $competition) {
+
+                    $divisionids = json_decode($competition->divisionids);
+                    $divisionids = implode(',', (array) $divisionids);
+                    $competition->divisons = DB::select("
+                        SELECT `divisionid`,`label`,`code`
+                        FROM `divisions`
+                        WHERE `divisionid` IN (". $divisionids .") 
+                    ");
+                    $competition->divisionids = null;
+
+                    $roundids = json_decode($competition->roundids);
+                    $roundids = implode(',', (array) $roundids);
+                    $competition->rounds = DB::select("
+                        SELECT *
+                        FROM `rounds`
+                        WHERE `roundid` IN (". $roundids .")
+                    ");
+                    $competition->roundids = null;
+                }
+
+            }
+
         }
         
         return response()->json([
