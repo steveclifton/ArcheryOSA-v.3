5aa9e26a044a841357a462699efdea26bf65579b
Steve <steve.clifton@outlook.com>
2018-11-22 22:29:46 +1300

    Add requests

---
+++ app/Http/Requests/User/CreateChild.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateChild extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        return [
+            'firstname'  => 'required|max:55',
+            'lastname'   => 'required|max:55',
+            'email'      => 'nullable|email|max:155',
+
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'firstname.required' => 'First name is required',
+            'lastname.required'  => 'Last name is required',
+        ];
+    }
+}
---
+++ app/Http/Requests/User/UpdateChild.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateChild extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        return [
+            'firstname'  => 'required|max:55',
+            'lastname'   => 'required|max:55',
+            'email'      => 'nullable|max:155',
+            'username'   => 'required'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'firstname.required' => 'First name is required',
+            'lastname.required'  => 'Last name is required',
+            'username.required'  => 'Error with page'
+        ];
+    }
+}
