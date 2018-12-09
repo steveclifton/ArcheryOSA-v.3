02027088b7a3fa72fa11b794a396f63bcfb3d16d
Steve <steve.clifton@outlook.com>
2018-12-09 19:12:55 +1300

    Update

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'archeryosa@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'ArcheryOSA'),
+        'address' => env('MAIL_FROM_ADDRESS', ''),
+        'name' => env('MAIL_FROM_NAME', ''),
     ],
 
     /*
