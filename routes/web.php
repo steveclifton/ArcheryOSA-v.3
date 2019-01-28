1f1f5a9b7faefc5ac9d9e6c2c082d72b87256a87
Steve <steve.clifton@outlook.com>
2019-01-28 16:36:09 +1300

    Add downloads route

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-//admin routes
+// Downloads
+require_once 'downloads.php';
 
-//Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+
 
 
 Route::get('/events', 'Events\PublicEvents\EventController@getAllEvents');
