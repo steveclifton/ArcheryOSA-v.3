205a6dbc3d40442380a15269bba2d67064654cf5
Steve <steve.clifton@blackpepper.co.nz>
2019-05-15 22:05:53 +1200

    Add errors into details

--- resources/views/events/public/details.blade.php
+++ resources/views/events/public/details.blade.php
@@ -20,38 +20,24 @@
 
     <div class="row">
 
+
         <div class="card-box ">
+            @include('template.alerts')
             <div class="row ">
                 <div class="col-lg-6">
 
-                    {{--<p class="text-muted m-b-30 font-16">Sponsors and prizes </p>--}}
-
                     <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-                        {{--<ol class="carousel-indicators">--}}
-                            {{--<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>--}}
-                        {{--</ol>--}}
+
                         <div class="carousel-inner" role="listbox">
                             <div class="carousel-item active">
                                 <img class="d-block img-fluid" src="{{URL::asset('/images/events/' . $event->imagedt)}}"
                                      alt="First slide"/>
-                                {{--<div class="carousel-caption d-none d-md-block">--}}
-                                    {{-- <h3 class="text-white">First slide label</h3> --}}
-                                    {{-- <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> --}}
-                                {{--</div>--}}
+
                             </div>
 
 
                         </div>
-                        {{--<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button"--}}
-                           {{--data-slide="prev">--}}
-                            {{--<span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
-                            {{--<span class="sr-only">Previous</span>--}}
-                        {{--</a>--}}
-                        {{--<a class="carousel-control-next" href="#carouselExampleCaptions" role="button"--}}
-                           {{--data-slide="next">--}}
-                            {{--<span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
-                            {{--<span class="sr-only">Next</span>--}}
-                        {{--</a>--}}
+
                     </div>
                     <div class="d-flex justify-content-around row" id="myWidgetRow">
                         <div class="widget-bg-color-icon card-box col-lg-5 " id="myUserWidget">
