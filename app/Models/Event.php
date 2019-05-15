ffbed9d8247bc35d4fab51fadba280eb7c7a52da
Steve <steve.clifton@blackpepper.co.nz>
2019-05-15 22:03:22 +1200

    Add methods to check for event status

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Event extends Model
 {
@@ -24,6 +25,64 @@ class Event extends Model
         return $this->eventtypeid == 3;
     }
 
+    public function canEnterEvent()
+    {
 
+        // check only for events, LEAGUE is different
+        if (!empty($this->attributes['visible']) && $this->isEvent() && $this->eventstatusid === 1) {
+
+            // get the eventcomps, if empty, false
+            $eventcompetitions = EventCompetition::where('eventid', $this->eventid)->get()->first();
+            if (empty($eventcompetitions)) {
+                return false;
+            }
+
+
+            // if we are over entry limit
+            $entrycount = DB::table('evententrys')
+                            ->where('eventid', $this->eventid)
+                            ->count();
+
+            if (!empty($this->entrylimit) && $entrycount >= $this->entrylimit) {
+                return false;
+            }
+
+            // set the date default to be nz
+            date_default_timezone_set('NZ');
+
+            // if we have an entry close set, do checks
+            if (!empty($this->entryclose)) {
+
+                if (time() > (strtotime($this->entryclose)) // if entry close is after closing date
+                    ||
+                    (strtotime($this->entryclose) > strtotime($this->start) + 86400) // if the entry close is after the start date + 1 day
+                ) {
+                    return false;
+                }
+
+            }
+
+
+            // make it based off the end date + 1day, not start date. Allows people to join on the day until the end
+            if (time() > (strtotime($this->end) + 60*60*24) ) {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        return false;
+    }
+
+    public function canEnterLeague()
+    {
+        // make it based off the end date + 1day, not start date. Allows people to join on the day until the end
+        if (time() > (strtotime($this->end) + 60*60*24) ) {
+            return false;
+        }
+
+        return (!empty($this->attributes['visible']) && $this->eventstatusid === 1);
+    }
 
 }
