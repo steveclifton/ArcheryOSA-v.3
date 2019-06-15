6c8074fafcd904a2c8d7b8ee744e0f122d7bb222
Steve <steve.clifton@blackpepper.co.nz>
2019-06-15 19:27:28 +1200

    Remove unvisible events

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -42,6 +42,7 @@ class HomeController extends Controller
                 JOIN `evententrys` ee USING (`eventid`)
                 JOIN `entrystatus` es ON (ee.entrystatusid = es.entrystatusid)
                 WHERE `ee`.`userid` = '".Auth::id()."'
+                AND `e`.`visible` = 1
                 AND `e`.`end` > now()
                 ORDER BY e.start
             ");
