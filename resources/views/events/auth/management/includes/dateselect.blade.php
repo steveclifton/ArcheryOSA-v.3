cbdca0c467bedd1e9a8043169dda268e47be3334
Steve <steve.clifton@blackpepper.co.nz>
2019-07-27 14:41:32 +1200

    Add event competition sequencing

--- resources/views/events/auth/management/includes/dateselect.blade.php
+++ resources/views/events/auth/management/includes/dateselect.blade.php
@@ -11,3 +11,18 @@
         </select>
     </div>
 </div>
+
+<div class="form-group row">
+    <label class="col-sm-12 col-md-3 col-form-label">Competition Sequence</label>
+    <div class="col-md-9">
+        <select name="sequence" class="form-control">
+            @foreach(range(1, count($eventcompetitions) + 1) as $i)
+                <option value="{{$i}}"
+                        {!! (old('sequence') ?? $competition['sequence'] ?? (count($eventcompetitions) + 1) ) == $i ? 'selected' : ''!!}>
+                    {{ $i }}
+                </option>
+            @endforeach
+        </select>
+        <span>The order in which this competition occurs in the event</span>
+    </div>
+</div>
