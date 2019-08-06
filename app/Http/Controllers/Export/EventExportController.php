89f1c44a26c4c19708a393ffc27b1ed00d598420
Steve <steve.clifton@blackpepper.co.nz>
2019-08-06 21:08:36 +1200

    Fix country name

--- app/Http/Controllers/Export/EventExportController.php
+++ app/Http/Controllers/Export/EventExportController.php
@@ -61,7 +61,7 @@ class EventExportController extends Controller
                    ee.country as countrycode, 
                    (SELECT `name` FROM `countries` WHERE `iso_3166_3` = ee.country) as country,
                    DATE_FORMAT(str_to_date(ee.dateofbirth, '%d-%m-%Y'),'%Y-%m-%d') as dateofbirth,
-                   (SELECT `name` FROM `countries` WHERE `iso_3166_2` = ee.country) as subclass,
+                   (SELECT `iso_3166_2` FROM `countries` WHERE `iso_3166_3` = ee.country) as subclass,
                    c.description as clubcode,
                    c.label as clubname
             FROM `evententrys` ee
@@ -76,7 +76,7 @@ class EventExportController extends Controller
             ORDER BY `d`.label, ee.firstname
         ", ['eventid' => $event->eventid]);
 
-
+        dd($entrys);
         foreach ($entrys as $entry) {
 
             if ($entry->gender == 'f') {
