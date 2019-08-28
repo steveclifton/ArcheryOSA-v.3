5d76031672a5a4f55698dbe22346824813043f1b
Steve <steve.clifton@outlook.com>
2019-08-28 22:05:03 +1200

    Remove telescope

--- resources/views/template/scripts.blade.php
+++ resources/views/template/scripts.blade.php
@@ -9,18 +9,3 @@
 <script src="{{URL::asset('/js/jquery.core.js')}}"></script>
 <script src="{{URL::asset('/js/jquery.app.js')}}"></script>
 <script src="{{URL::asset('/js/archeryosa.js')}}"></script>
-
-{{--<script src="{{URL::asset('/serviceworker.js')}}"></script>--}}
-{{--<script>--}}
-{{--    window.addEventListener('load', async e => {--}}
-{{--        if ('serviceWorker' in navigator) {--}}
-{{--            try {--}}
-{{--                navigator.serviceWorker.register('serviceWorker.js');--}}
-{{--            }--}}
-{{--            catch (error) {--}}
-{{--                console.log('SW failed');--}}
-{{--            }--}}
-{{--        }--}}
-{{--    });--}}
-
-{{--</script>--}}
