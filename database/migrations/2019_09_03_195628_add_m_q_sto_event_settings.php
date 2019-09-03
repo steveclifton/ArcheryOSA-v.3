3a27b8301e7b625a7226053142477e0d1e54adc3
Steve <steve.clifton@outlook.com>
2019-09-03 21:20:45 +1200

    Add migration

---
+++ database/migrations/2019_09_03_195628_add_m_q_sto_event_settings.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMQStoEventSettings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function($table) {
+            $table->integer('mqs')->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
