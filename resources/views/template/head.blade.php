5c77b81421c09950c50be5110473978e0c54f2bf
Steve <steve.clifton@outlook.com>
2019-09-06 09:44:31 +1200

    SEO updates

--- resources/views/template/head.blade.php
+++ resources/views/template/head.blade.php
@@ -1,11 +1,21 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-<meta name="description" content="Archery Online Scoring Application"/>
+<meta name="description" content="Archery OSA is a free online archery event management system which allows you to run your archery events completely online"/>
+<meta name="referrer" content="always">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
 <meta property="og:image" content="{{URL::asset('/images/archeryosablack.jpg')}}">
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="ArcheryOSA" />
+<meta property="og:description" content="Archery OSA is a free online archery event management system which allows you to run your archery events completely online" />
+<meta property="og:url" content="https://archeryosa.com/" />
+<meta property="og:site_name" content="ArcheryOSA" />
 
 <link rel="icon" href="{{URL::asset('/images/favion.ico')}}" type="image/x-icon" />
 <link rel="shortcut icon" href="{{URL::asset('/images/favion.ico')}}" type="image/x-icon" />
+<link rel="canonical" href="https://archeryosa.com/" itemprop="url">
+<link rel="canonical" href="https://archeryosa.com/">
 
 <link rel="apple-touch-icon" href="touch-icon-iphone.png">
 <link rel="apple-touch-icon" sizes="144x144" href="/icon-144x144.png">
@@ -17,10 +27,8 @@
 
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
-
 <script src='https://www.google.com/recaptcha/api.js'></script>
 
-{{--<link rel="manifest" href="/manifest.json">--}}
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109735416-1"></script>
@@ -31,8 +39,6 @@
 
     gtag('config', 'UA-147182984-1');
 </script>
-
-
 
 <!-- Google Tag Manager -->
 <script>
