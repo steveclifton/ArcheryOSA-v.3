b0c8a53c3b747f4544bf9e4d22078ca9c4ae849e
Steve <steve.clifton@outlook.com>
2019-09-05 14:19:07 +1200

    Update GTM

--- resources/views/template/default.blade.php
+++ resources/views/template/default.blade.php
@@ -9,6 +9,12 @@
 
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PL2ND3T"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     @include('template.navigation')
 
--- resources/views/template/head.blade.php
+++ resources/views/template/head.blade.php
@@ -29,16 +29,18 @@
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-109735416-1');
+    gtag('config', 'UA-147182984-1');
 </script>
 
 
 
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-K87KGQS');
+    })(window,document,'script','dataLayer','GTM-PL2ND3T');
 </script>
 <!-- End Google Tag Manager -->
+
