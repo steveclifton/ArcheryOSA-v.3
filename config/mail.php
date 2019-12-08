e1e998403373e77f1430a45936db872bf1520d0d
Steve <steve.clifton@outlook.com>
2019-12-08 19:45:51 +1300

    remove

--- config/mail.php
+++ config/mail.php
@@ -2,15 +2,6 @@
 
 return [
 
-
-    'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-        ],
-    ],
-
     /*
     |--------------------------------------------------------------------------
     | Mail Driver
