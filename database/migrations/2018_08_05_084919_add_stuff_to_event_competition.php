4681dc514c3891db6da593065a15773cdbb4b50c
Steve <steve.clifton@outlook.com>
2020-01-01 18:09:06 +1300

    Fix migration

--- database/migrations/2018_08_05_084919_add_stuff_to_event_competition.php
+++ database/migrations/2018_08_05_084919_add_stuff_to_event_competition.php
@@ -14,8 +14,8 @@ class AddStuffToEventCompetition extends Migration
     public function up()
     {
         Schema::table('eventcompetitions', function($table) {
-            $table->text('competitionids')->default(0)->nullable();
-            $table->text('divisionids')->default(0)->default(0)->nullable();
+            $table->text('competitionids')->nullable();
+            $table->text('divisionids')->nullable();
             $table->integer('ignoregenders')->default(0);
         });
     }
