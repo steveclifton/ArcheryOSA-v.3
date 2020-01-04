f02cf9272d848d2914d332c01cf0a8df3d0821af
Steve <steve.clifton@outlook.com>
2020-01-04 16:39:07 +1300

    Add privacy page

---
+++ resources/views/static/privacy.blade.php
@@ -0,0 +1,105 @@
+@extends('template.default')
+
+@section ('title')Privacy @endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card-box">
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="text-center">
+                            <h4 class="font-600">Privacy Policy</h4>
+                            <p class="text-muted">
+                                Last updated: 4 Jan 2020<br>
+                            </p>
+                        </div>
+                    </div><!-- end col -->
+                </div><!-- end row -->
+
+                <div class="row" style="margin-top: 40px">
+
+                    <div>
+                        <h4>Terms</h4>
+                        <p>ArcheryOSA ("us", "we", or "our") operates ArcheryOSA (the "Site"). </p>
+                    </div>
+
+                    <div>
+                        <h4>Purpose</h4>
+                        <p>This page informs you of our policies regarding the collection, use and disclosure of Personal Information we receive from users of the Site.</p>
+                        <p>We use your Personal Information only for providing and improving the Site. By using the Site, you agree to the collection and use of information in accordance with this policy.</p>
+                    </div>
+
+                    <div>
+                        <h4>Information Collection And Use</h4>
+                        <p>While using our Site, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you.</p>
+                        <p>Personally identifiable information may include, but is not limited to your name, email address, phone number, address and date of birth</p>
+                        <p>Personal information you provide will be shared with the organisers of events you enter, and where appropriate, with Archery New Zealand Incorporated.</p>
+                    </div>
+
+                    <div>
+                        <h4>Log Data</h4>
+                        <p>
+                            Like many site operators, we collect information that your browser sends whenever you visit our Site ("Log Data").
+                        </p>
+                        <p>
+                            This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser type, browser version, the pages of our Site that you visit, the time and date of your visit, the time spent on those pages and other statistics.
+                        </p>
+                        <p>
+                            In addition, we may use third party services such as Google Analytics that collect, monitor and analyze this in order to better serve our Site to our customers.
+                        </p>
+                    </div>
+
+                    <div>
+                        <h4>Communications</h4>
+                        <p>
+                        You may receive event updates from ArcheryOSA for operational reasons (such as password resetting), or for events that you have involvement with.
+                        </p>
+                    </div>
+
+                    <div>
+                        <h4>Cookies</h4>
+                        <p>
+                        Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer's hard drive.
+                        </p>
+                        <p>
+                        Like many sites, we use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Site.
+                        </p>
+                        <p>
+                            We do not store any passwords using "cookies" on your computer.
+                        </p>
+                    </div>
+
+                    <div>
+                        <h4>Children</h4>
+                        <p>
+                            We consider it is the responsibility of parents to monitor their children's use of our Site, and provide instructions as to what infomation is suitable to provide when registering for an event.
+                        </p>
+
+                    </div>
+
+                    <div>
+                        <h4>Security</h4>
+                        <p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+                        <p>When you enter your personal information on our website we use Secure Sockets Layer (SSL) technology to protect your information as it is transmitted to us.</p>
+                    </div>
+
+                    <div>
+                    <h4>Changes To This Privacy Policy</h4>
+                        <p>This Privacy Policy is effective as of 4 Jan 2020 and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.</p>
+                        <p>We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. Your continued use of the Service after we post any modifications to the Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.</p>
+                        <p>If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.</p>
+                    </div>
+
+                    <div>
+                    <h4>Contact Us</h4>
+                    <p>You have the right to ask for a copy of any personal information we hold about you, and to ask for it to be corrected if you think it is wrong. If you’d like to ask for a copy of your information, or to have it corrected, please contact us at info@archeryosa.com.</p>
+                    <p>ArcheryOSA is an open-source web application, which means the source code that the website operates from can be found at https://github.com/steveclifton/ArcheryOSA-v.3</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
