a193556593068df3c2bedfa8cba296a37a79eb14
Steve <steve.clifton@outlook.com>
2020-01-07 09:57:55 +1300

    Fix image scaling

--- resources/views/events/results/results.blade.php
+++ resources/views/events/results/results.blade.php
@@ -17,47 +17,53 @@
                 <h4 class="page-title">
                     <a href="/event/results/{{$event->eventurl}}">{{$event->label}}</a>
                     /
-                    <a href="javascript:;">{{ucwords($eventcompetition->label)}}</a>
+                    <a href="javascript:;">{{ucwords($eventcompetition->label ?? '')}}</a>
                 </h4>
 	    	</div>
 		</div>
 	</div>
     @if (!empty($event->imagebanner))
         <div class="col-md-12 homePageBanner">
-        <div class="panel panel-default text-center d-lg-none text-white slider-bg m-b-0"
-             style="background-position:center !important; background-size:contain !important; background-size: cover !important; background-repeat: no-repeat;  width: 100%; background: url({{asset('images/events/' . $event->imagebanner)}});">
-            <div class="slider-overlay br-radius"></div>
-            <div class="panel-body p-0">
-                <div class="">
-                    <div id="owl-slider-2" class="owl-carousel">
-                        <div class="item">
-                            <h1>
-                                <a href="#" class="text-white font-600">{{ucwords($event->label)}}</a>
-                            </h1>
-                            <p class="m-t-30"><em>{{ucwords($eventcompetition->label)}}</em></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+            <div class="panel panel-default text-center d-lg-none text-white slider-bg m-b-0"
+                 style="background-position:center !important;
+                         background-size:contain !important;
+                         background-size: cover !important;
+                         background-repeat: no-repeat;
+                         width: 100%;
+                         background: url({{asset('images/events/' . $event->imagedt)}});">
+                <div class="slider-overlay br-radius"></div>
+                <div class="panel-body p-0">
+                    <div class="">
+                        <div id="owl-slider-2" class="owl-carousel">
+                            <div class="item">
+                                <h3><a href="#" class="text-white font-600 archeryHeadText">{{ucwords($eventcompetition->label)}}</a></h3>
 
-        <div class="panel panel-default text-center desktopOnlyImg d-none d-lg-block text-white slider-bg m-b-0"
-             style="background-position:center !important; background-size:contain !important; background-size: cover !important; background-repeat: no-repeat;  width: 100%; background: url({{asset('images/events/' . $event->imagebanner)}});">
-            <div class="slider-overlay br-radius"></div>
-            <div class="panel-body p-0">
-                <div class="">
-                    <div id="owl-slider-2" class="owl-carousel">
-                        <div class="item">
-                            <h1>
-                                <a href="#" class="text-white font-600">{{ucwords($event->label)}}</a>
-                            </h1>
-                            <p class="m-t-30"><em>{{ucwords($eventcompetition->label)}}</em></p>
+                                <p class="m-t-30"><em></em></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+
+            <div class="panel panel-default text-center desktopOnlyImg d-none d-lg-block text-black slider-bg m-b-0"
+                 style="background-position:center !important;
+                         background-size:contain !important;
+                         background-size: cover !important;
+                         background-repeat: no-repeat;
+                         width: 100%; background: url({{asset('images/events/' . $event->imagedt)}});">
+                <div class="slider-overlay br-radius"></div>
+                <div class="panel-body p-0">
+                    <div class="">
+                        <div id="owl-slider-2" class="owl-carousel">
+                            <div class="item">
+                                <h3><a href="#" class=" archeryHeadText">{{ucwords($eventcompetition->label)}}</a></h3>
+                                <p class="m-t-30"><em></em></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- col-->
     @endif
 
 	<div class="row">
