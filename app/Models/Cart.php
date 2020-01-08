445978731472bf4c9fba66470d96384a091f9419
Steve <steve.clifton@outlook.com>
2020-01-08 21:24:04 +1300

    Add cart model

---
+++ app/Models/Cart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    protected $table = 'persistentcart';
+    protected $primaryKey = 'cartid';
+
+
+}
