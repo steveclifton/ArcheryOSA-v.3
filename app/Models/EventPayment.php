6439b7d31c1214285e93426806b155515f5a3ac2
Steve <steve.clifton@outlook.com>
2020-01-08 21:24:13 +1300

    Add event payment model

---
+++ app/Models/EventPayment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventPayment extends Model
+{
+    protected $table = 'eventpayments';
+    protected $primaryKey = 'eventpaymentid';
+}
