23d54b850ad85e1ebc2c1e618cfbd7708ad94458
Steve <steve.clifton@outlook.com>
2020-01-12 07:07:13 +1300

    Update markup

--- resources/views/events/public/registration/createregistration.blade.php
+++ resources/views/events/public/registration/createregistration.blade.php
@@ -239,35 +239,29 @@
 
                 @if (!empty($event->mqs))
                     <hr>
-                    @php $oldmqs = old('mqs') @endphp
+                    @php $oldmqs = old('mqs'); @endphp
                     <h4 class="m-t-0 m-b-30 text-center addFormHeader header-title">MQS Scores Required</h4>
 
                     <div class="form-group row">
                         <label class="col-sm-12 col-md-3 col-form-label">MQS Score 1*</label>
                         <div class="col-md-6">
-                            <input name="mqs[]" type="text" class="form-control" value="{{!empty($oldmqs[0]) ? $oldmqs[0] : 0 }}" required >
-                        <span class="help-block"><small>Leave as 0 if not applicable</small></span>
-
+                            <input name="mqs[]" type="text" class="form-control" value="{{ !empty($oldmqs[0]) ? $oldmqs[0] : 0 }}" required >
+                            <span class="help-block"><small>Leave as 0 if not applicable</small></span>
                         </div>
-
                     </div>
                     <div class="form-group row">
                         <label class="col-sm-12 col-md-3 col-form-label">MQS Score 2*</label>
                         <div class="col-md-6">
-                            <input name="mqs[]" type="text" class="form-control" value="{{!empty($oldmqs[1]) ? $oldmqs[1] : 0 }}" required >
-                        <span class="help-block"><small>Leave as 0 if not applicable</small></span>
-
+                            <input name="mqs[]" type="text" class="form-control" value="{{ !empty($oldmqs[1]) ? $oldmqs[1] : 0 }}" required >
+                            <span class="help-block"><small>Leave as 0 if not applicable</small></span>
                         </div>
-
                     </div>
                     <div class="form-group row">
                         <label class="col-sm-12 col-md-3 col-form-label">MQS Score 3*</label>
                         <div class="col-md-6">
-                            <input name="mqs[]" type="text" class="form-control" value="{{!empty($oldmqs[2]) ? $oldmqs[2] : 0 }}" required >
-                        <span class="help-block"><small>Leave as 0 if not applicable</small></span>
-
+                            <input name="mqs[]" type="text" class="form-control" value="{{ !empty($oldmqs[2]) ? $oldmqs[2] : 0 }}" required >
+                            <span class="help-block"><small>Leave as 0 if not applicable</small></span>
                         </div>
-
                     </div>
                 @endif
 
@@ -293,13 +287,22 @@
                     </div>
 
                 @endif
-                <hr>
+
+                <h5 style="text-align: center; padding-top: 40px; padding-bottom: 20px">Payment Type</h5>
+                <div class="form-group row" style="padding-bottom: 40px">
+                    <label class="col-sm-12 col-md-3 col-form-label">Payment Type</label>
+                    <div class="col-md-9">
+                        <select name="paymenttype" class="form-control" id="paymentType">
+                            <option value="bt">Bank Transfer</option>
+                        </select>
+                    </div>
+                </div>
+
                 <div class="form-group mb-0 justify-content-start row">
                     <div class="col-sm-12 col-md-3 col-form-label"></div>
-                    <div class="col-3">
-                        <button type="submit" class="myButton btn btn-inverse btn-info waves-effect waves-light">Enter</button>
+                    <div class="col-sm-12 col-md-12 text-center">
+                        <button type="submit" class="myButton btn btn-success">Enter</button>
                     </div>
-
                 </div>
 
             </form>
