331f3dc274679bfd9d0404ed257bccae6d20a7bd
Steve <steve.clifton@outlook.com>
2020-03-29 19:10:15 +1300

    Update view

--- resources/views/events/results/league/leagueresults-overall.blade.php
+++ resources/views/events/results/league/leagueresults-overall.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
 
+
     <link href="{{URL::asset('/plugins/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
     <link href="{{URL::asset('/plugins/datatables/buttons.bootstrap4.min.css')}}"    rel="stylesheet">
     <link href="{{URL::asset('/plugins/datatables/responsive.bootstrap4.min.css')}}" rel="stylesheet">
@@ -24,69 +25,63 @@
         @include('events.results.templates.event-banners')
     @endif
 
+    <style>
+        body {
+            line-height: 1;
+            font-size: 12px;
+        }
+
+        table.dataTable {
+            margin-top: 1px !important;
+            margin-bottom: 20px !important;
+        }
+    </style>
+
 	<div class="row">
         <div class="col-lg-12">
             <ul class="nav nav-tabs tabs"></ul>
-
-            <div class="tab-content">
+            <div class="tab-content" style="padding: 0%">
                 @foreach ($evententrys as $bowtype => $ee)
                     <div class="tab-pane active" id="{{$bowtype}}">
                         @foreach($ee as $division => $archers)
-                            <h5 class="tableTitle d-block d-sm-block d-md-block d-lg-none">{{$division}}</h5>
-                            @php $data = reset($archers); @endphp
-
-                                <div class="table-responsive">
-                                    <table class="table table-striped table-bordered datatable-buttons" cellspacing="0" width="100%">
-                                        <thead>
-                                            <tr id="tabltr">
-                                                <th class="d-lg-none">Archer</th>
-                                                <th class="d-none d-sm-none d-md-none d-lg-block d-xl-block">{{$division}}</th>
-                                                <th>Average</th>
-                                                <th>Top 10 Total</th>
-                                                <th>Top 10 Points</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach ($archers as $archer)
-                                            <tr class="results">
-                                                <th scope="row" width="15%">
-                                                    <a href="/profile/public/{{$archer->username}}">
-                                                        {{ucwords($archer->firstname . ' ' . $archer->lastname)}}
-                                                    </a>
-                                                </th>
-                                                <td width="10%">
-                                                    {{number_format($archer->average->average ?? 0, 2)}}
-                                                </td>
-                                                <td width="10%">
-                                                    {{intval($archer->top10->total ?? 0)}}
-                                                </td>
-                                                <td width="10%">
-                                                    {{intval($archer->top10points->points ?? 0)}}
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-                                    <hr>
-                                </div>
-
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered datatable-buttons" cellspacing="0" width="100%">
+                                    <thead>
+                                        <tr id="tabltr">
+                                            <th>{{$division}}</th>
+                                            <th>Average</th>
+                                            <th>Top 10 Total</th>
+                                            <th>Top 10 Points</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach ($archers as $archer)
+                                        <tr class="results">
+                                            <th scope="row" width="15%">
+                                                <a href="/profile/public/{{$archer->username}}">
+                                                    {{ucwords($archer->firstname . ' ' . $archer->lastname)}}
+                                                </a>
+                                            </th>
+                                            <td width="10%">
+                                                {{number_format($archer->average->average ?? 0, 2)}}
+                                            </td>
+                                            <td width="10%">
+                                                {{intval($archer->top10->total ?? 0)}}
+                                            </td>
+                                            <td width="10%">
+                                                {{intval($archer->top10points->points ?? 0)}}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         @endforeach
                     </div>
                 @endforeach
             </div>
-
         </div>
     </div>
-
-
-
-
-
-
-
-
-
-
 
     <script src="{{URL::asset('/plugins/datatables/jquery.dataTables.min.js')}}"></script>
     <script src="{{URL::asset('/plugins/datatables/dataTables.bootstrap4.min.js')}}"></script>
@@ -107,17 +102,14 @@
         $(document).ready(function () {
             //Buttons examples
             var index = $('#tabltr').find('th:last').index();
+
             var table = $('.datatable-buttons').DataTable({
                 lengthChange: false,
                 bPaginate: false,
                 bInfo : false,
                 searching : false,
                 "order": [[ index, "desc" ], [index -1, "desc"]]
-                // buttons: ['excel', 'pdf']
             });
-            //
-            // table.buttons().container()
-            //     .appendTo('.datatable-buttons_wrapper .col-md-6:eq(0)');
         });
     </script>
 
