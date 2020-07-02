c2cb8f3527b522c0d4353a9dd306553f5880b91f
Steve <steve.clifton@outlook.com>
2020-07-02 14:57:56 +1200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -168,5 +168,33 @@
         </div>
     </div>
 
+    <button type="button" style="display: none"
+            data-toggle="modal"
+            data-target="#newsmodal"></button>
+
+    <div id="newsmodal" class="modal fade"
+         tabindex="-1" role="dialog"
+         aria-labelledby="full-width-modalLabel"
+         aria-hidden="true" style="display: none;">
+
+        <div class="modal-dialog ">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="full-width-modalLabel">News</h4>
+                    <button type="button" class="close" data-dismiss="modal" data-hash="{{md5($message)}}" aria-hidden="true">×</button>
+                </div>
+                <div class="modal-body">
+                    {!! $message  !!}
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary waves-effect" data-dismiss="modal" data-hash="{{md5($message)}}">
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{URL::asset('/js/home.js')}}"></script>
 
 @endsection
