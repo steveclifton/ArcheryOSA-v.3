09173c2e9c9ef510d31eb82865abf25de2f9bf21
Steve <steve.clifton@outlook.com>
2020-07-05 13:05:15 +1200

    Update to use include

--- resources/views/events/public/open.blade.php
+++ resources/views/events/public/open.blade.php
@@ -3,70 +3,8 @@
 @section ('title')Upcoming Events @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="page-title-box">
-                <h4 class="page-title "><a href="/events">Upcoming Events</a></h4>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
 
-            <div class="card-columns">
-                @foreach(array_slice($events, 0, 3) as $event)
-                    <a href="/event/details/{{$event->eventurl}}">
-                        <div class="card m-b-20">
-                            <img class="card-img-top img-fluid" src="{{URL::asset('/images/events/' . $event->imagedt)}}"
-                                 alt="{{$event->label}}">
-                            <div class="card-body">
-                                <h4 class="card-title font-18 mt-0">{{$event->label}}</h4>
-                                <p class="card-text">Start : {!! date('d F Y', strtotime($event->start)) !!}</p>
-                                @php $date = (!empty($event->entryclose) && $event->entryclose != '1970-01-01') ? date('d F Y', strtotime($event->entryclose)) : 'Not Specified';  @endphp
-                                <p class="card-text">Entries Close : {!! $date !!}</p>
-                                <p class="card-text">Event Level : {!! $event->level !!}</p>
-                                <p class="card-text">Region : {!! $event->region !!}</p>
-                            </div>
-                        </div>
-                    </a>
-                @endforeach
-            </div>
-        </div>
-    </div>
+    @include('events.public.include.upcoming')
 
-    @if(!empty(array_slice($events, 3)))
 
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card-box">
-
-                    @if (!empty(array_slice($events, 1)))
-                        <div class="table-responsive">
-                            <table class="table table-hover">
-                                <thead class="thead-light">
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Event Level</th>
-                                    <th>Start</th>
-                                    <th>Region</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach(array_slice($events, 3) as $event)
-                                    <tr>
-                                        <th scope="row"><a href="/event/details/{{$event->eventurl}}">{{$event->label}}</a></th>
-                                        <td>{!! $event->level !!}</td>
-                                        <td>{{date('d F Y', strtotime($event->start))}}</td>
-                                        <td>{{$event->region}}</td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    @endif
-
-                </div>
-            </div>
-        </div>
-    @endif
 @endsection
--- resources/views/events/results/events-list.blade.php
+++ resources/views/events/results/events-list.blade.php
@@ -3,51 +3,22 @@
 @section ('title')Previous Events @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="page-title-box">
-                <h4 class="page-title">
-                    <a href="javascript:;">Events Results</a>
-                </h4>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
+    @if(!empty($events))
 
-            <div class="card-columns">
-                @foreach(array_slice($events, 0, 3) as $event)
-                    <a href="/event/results/{{$event->eventurl}}">
-                        <div class="card m-b-20">
-                            <img class="card-img-top img-fluid" src="{{URL::asset('/images/events/' . $event->imagedt)}}"
-                                 alt="{{$event->label}}">
-                            <div class="card-body">
-                                <h4 class="card-title font-18 mt-0">{{$event->label}}</h4>
-                                <p class="card-text">Start : {!! date('d F Y', strtotime($event->start)) !!}</p>
-                                <p class="card-text">Finish : {!! date('d F Y', strtotime($event->end)) !!}</p>
-                            </div>
-                        </div>
-                    </a>
-                @endforeach
-            </div>
-        </div>
-    </div>
-
-    @if(!empty(array_slice($events, 3)))
-        <!-- Section-Title -->
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="page-title-box">
-                    <h4 class="page-title">Event Results</h4>
-                </div>
-            </div>
-        </div>
-        <div class="row">
+        <div class="row" style="padding-top: 20px">
             <div class="col-lg-12">
-                <div class="card-box card-box-small">
+                <ul class="nav nav-tabs tabs">
+                    <li class="nav-item tab">
+                        <a href="javascript:;" data-toggle="tab" aria-expanded="true" class="nav-link show">
+                            Completed Events
+                        </a>
+                    </li>
+                </ul>
 
-                    @if (!empty(array_slice($events, 1)))
-                        <div class="table-responsive">
+                <div class="tab-content">
+
+                    @if (!empty($events))
+                        <div class="table-responsive text-nowrap">
                             <table class="table table-hover">
                                 <thead class="thead-light">
                                 <tr>
@@ -57,7 +28,7 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @foreach(array_slice($events, 3) as $event)
+                                @foreach($events as $event)
                                     <tr>
                                         <th scope="row"><a href="/event/results/{{$event->eventurl}}">{{$event->label}}</a></th>
                                         <td>{{date('d M Y', strtotime($event->start))}}</td>
