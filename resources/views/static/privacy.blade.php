fb5068a8cde92a760734843cfd14c1dd86fc574b
Steve <steve.clifton@outlook.com>
2020-07-15 17:19:59 +1200

    Update privacy.blade.php

--- resources/views/static/privacy.blade.php
+++ resources/views/static/privacy.blade.php
@@ -12,7 +12,7 @@
                         <div class="text-center">
                             <h4 class="font-600">Privacy Policy</h4>
                             <p class="text-muted">
-                                Last updated: 4 Jan 2020<br>
+                                Last updated: 15 July 2020<br>
                             </p>
                         </div>
                     </div><!-- end col -->
@@ -22,7 +22,7 @@
 
                     <div>
                         <h4>Terms</h4>
-                        <p>ArcheryOSA ("us", "we", or "our") operates ArcheryOSA (the "Site"). </p>
+                        <p>ArcheryOSA ("us", "we", or "our") operates ArcheryOSA (the "Site"). This includes all pages accessable on the archeryosa.com and archeryosa.co.nz domains </p>
                     </div>
 
                     <div>
@@ -89,6 +89,7 @@
                     <h4>Changes To This Privacy Policy</h4>
                         <p>This Privacy Policy is effective as of 4 Jan 2020 and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.</p>
                         <p>We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. Your continued use of the Service after we post any modifications to the Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.</p>
+                        <p>If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.</p>
                         <p>If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.</p>
                     </div>
 
