e1a58098c8bcfd67ae65e2c856e06dbc470c7062
Steve <steve.clifton@outlook.com>
2024-01-08 20:55:49 +1300

    Update layout

--- resources/views/events/results/league/leaguecompetitions.blade.php
+++ resources/views/events/results/league/leaguecompetitions.blade.php
@@ -17,42 +17,40 @@
 
     <div class="row">
         <div class="col-lg-12">
-            <div class="card-box">
-                @include('template.alerts')
-                <div class="table-responsive">
-                    <table class="table table-hover">
-                        <thead class="thead-light">
-                            <tr>
-                                <th width="40%">Event Competition</th>
-                                <th width="40%">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if (!empty($overall))
-                                <tr>
-                                    <td scope="row" >
-                                        <a href="/event/results/{{$event->eventurl}}/overall">Overall</a>
-                                    </td>
-                                    <td scope="row">
-                                        <a href="/event/results/{{$event->eventurl}}/overall">See Results</a>
-                                    </td>
-                                </tr>
-                            @endif
-
-                        @foreach($rangeArr as $week)
+            @include('template.alerts')
+            <div class="table-responsive"  style="background: white; border: 2px solid lightgrey;">
+                <table class="table table-hover">
+                    <thead class="thead-light">
+                        <tr>
+                            <th width="40%">Event Competition</th>
+                            <th width="40%">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if (!empty($overall))
                             <tr>
                                 <td scope="row" >
-                                    <a href="/event/results/{{$event->eventurl}}/{{$week}}">Week {{$week}}</a>
+                                    <a href="/event/results/{{$event->eventurl}}/overall">Overall</a>
                                 </td>
                                 <td scope="row">
-                                    <a href="/event/results/{{$event->eventurl}}/{{$week}}">See Results</a>
+                                    <a href="/event/results/{{$event->eventurl}}/overall">See Results</a>
                                 </td>
                             </tr>
-                        @endforeach
+                        @endif
 
-                        </tbody>
-                    </table>
-                </div>
+                    @foreach($rangeArr as $week)
+                        <tr>
+                            <td scope="row" >
+                                <a href="/event/results/{{$event->eventurl}}/{{$week}}">Week {{$week}}</a>
+                            </td>
+                            <td scope="row">
+                                <a href="/event/results/{{$event->eventurl}}/{{$week}}">See Results</a>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
