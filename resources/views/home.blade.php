6e4bf213d3d139351f928fb16448fdc218412914
Steve <steve.clifton@outlook.com>
2024-01-10 19:43:21 +1300

    add ad

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -140,4 +140,18 @@
         <script src="{{URL::asset('/js/home.js')}}"></script>
     @endauth
 
+    @if(Auth::id() == 1)
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4820276379493226"
+                crossorigin="anonymous"></script>
+        <!-- Nav-Ad -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-4820276379493226"
+             data-ad-slot="1007855523"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    @endif
 @endsection
