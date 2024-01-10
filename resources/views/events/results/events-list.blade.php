568c13f1eb8a2b5b5db69c5ced5f2fac442a2501
Steve <steve.clifton@outlook.com>
2024-01-10 20:30:18 +1300

    Tweak ad styling on events

--- resources/views/events/results/events-list.blade.php
+++ resources/views/events/results/events-list.blade.php
@@ -75,4 +75,22 @@
             });
         </script>
     @endif
+
+
+    <div class="row" style="padding-bottom: 10px; height: auto !important;">
+        <div class="col-12 text-center mx-auto my-0 py-2 px-2" style="height: auto !important;">
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4820276379493226"
+                    crossorigin="anonymous"></script>
+            <!-- Previous-Events-Ad -->
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-client="ca-pub-4820276379493226"
+                 data-ad-slot="2303699289"
+                 data-ad-format="auto"
+                 data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+    </div>
 @endsection
